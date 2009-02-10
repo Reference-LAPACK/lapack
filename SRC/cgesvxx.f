e9454e224e5fa4d61076493be3916a64f274a5ae
deaglanhalligan <deaglanhalligan@8a072113-8704-0410-8d35-dd094bca7971>
2009-02-10 12:17:14 +0000

    cgesvxx and zgesvxx need at least 3*N WORK space after changes from last bug fix

--- SRC/cgesvxx.f
+++ SRC/cgesvxx.f
@@ -396,7 +396,7 @@
 *            is true, 0.0 is false.
 *         Default: 1.0 (attempt componentwise convergence)
 *
-*     WORK    (workspace) COMPLEX array, dimension (2*N)
+*     WORK    (workspace) COMPLEX array, dimension (3*N)
 *
 *     RWORK   (workspace) REAL array, dimension (3*N)
 *
