9043bc8ed0bc4951432e57e29d454b7f5abae287
julie <julie@8a072113-8704-0410-8d35-dd094bca7971>
2009-08-12 19:42:14 +0000

    Fix variables used

--- lapack/trunk/CheckTimeFunction.cmake
+++ lapack/trunk/CheckTimeFunction.cmake
@@ -7,22 +7,22 @@
 
 macro(CHECK_TIME_FUNCTION FUNCTION VARIABLE)
 
-    try_compile(${VARIABLE}
+    try_compile(RES
     ${PROJECT_BINARY_DIR}/INSTALL
     ${PROJECT_SOURCE_DIR}/INSTALL
     TIMING secondtst_${FUNCTION}
     OUTPUT_VARIABLE OUTPUT)
 
-    if(${VARIABLE})
+    if(RES)
       set(${VARIABLE} ${FUNCTION} CACHE INTERNAL "Have Fortran function ${FUNCTION}")
       message(STATUS "Looking for Fortran ${FUNCTION} - found")
       file(APPEND ${CMAKE_BINARY_DIR}${CMAKE_FILES_DIRECTORY}/CMakeOutput.log 
         "Fortran ${FUNCTION} exists. ${OUTPUT} \n\n")
-    else(${VARIABLE})
+    else(RES)
       message(STATUS "Looking for Fortran ${FUNCTION} - not found")
       file(APPEND ${CMAKE_BINARY_DIR}${CMAKE_FILES_DIRECTORY}/CMakeError.log 
         "Fortran ${FUNCTION} does not exist. \n ${OUTPUT} \n")
-    endif(${VARIABLE})
+    endif(RES)
 endmacro(CHECK_TIME_FUNCTION)
 
 
