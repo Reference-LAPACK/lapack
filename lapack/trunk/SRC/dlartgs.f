74065932de2acb84dfea56454f325d438e2fecc9
brian <brian@8a072113-8704-0410-8d35-dd094bca7971>
2010-11-03 23:02:29 +0000

    Added CS decomposition source files to SRC/

---
+++ lapack/trunk/SRC/dlartgs.f
@@ -0,0 +1,100 @@
+      SUBROUTINE DLARTGS( X, Y, SIGMA, CS, SN )
+      IMPLICIT NONE
+*
+*     Brian Sutton
+*     Randolph-Macon College
+*     July 2010
+*
+*     .. Scalar Arguments ..
+      DOUBLE PRECISION        CS, SIGMA, SN, X, Y
+*     ..
+*
+*  Purpose
+*  =======
+*
+*  DLARTGS generates a plane rotation designed to introduce a bulge in
+*  Golub-Reinsch-style implicit QR iteration for the bidiagonal SVD
+*  problem. X and Y are the top-row entries, and SIGMA is the shift.
+*  The computed CS and SN define a plane rotation satisfying
+*
+*     [  CS  SN  ]  .  [ X^2 - SIGMA ]  =  [ R ],
+*     [ -SN  CS  ]     [    X * Y    ]     [ 0 ]
+*
+*  with R nonnegative.  If X^2 - SIGMA and X * Y are 0, then the
+*  rotation is by PI/2.
+*
+*  Arguments
+*  =========
+*
+*  X       (input) DOUBLE PRECISION
+*          The (1,1) entry of an upper bidiagonal matrix.
+*
+*  Y       (input) DOUBLE PRECISION
+*          The (1,2) entry of an upper bidiagonal matrix.
+*
+*  SIGMA   (input) DOUBLE PRECISION
+*          The shift.
+*
+*  CS      (output) DOUBLE PRECISION
+*          The cosine of the rotation.
+*
+*  SN      (output) DOUBLE PRECISION
+*          The sine of the rotation.
+*
+*  ===================================================================
+*
+*     .. Parameters ..
+      DOUBLE PRECISION        NEGONE, ONE, ZERO
+      PARAMETER          ( NEGONE = -1.0D0, ONE = 1.0D0, ZERO = 0.0D0 )
+*     ..
+*     .. Local Scalars ..
+      DOUBLE PRECISION        R, S, THRESH, W, Z
+*     ..
+*     .. External Functions ..
+      DOUBLE PRECISION        DLAMCH
+      EXTERNAL           DLAMCH
+*     .. Executable Statements ..
+*
+      THRESH = DLAMCH('E')
+*
+*     Compute the first column of B'*B - SIGMA^2*I, up to a scale
+*     factor.
+*
+      IF( (SIGMA .EQ. ZERO .AND. ABS(X) .LT. THRESH) .OR.
+     $          (ABS(X) .EQ. SIGMA .AND. Y .EQ. ZERO) ) THEN
+         Z = ZERO
+         W = ZERO
+      ELSE IF( SIGMA .EQ. ZERO ) THEN
+         IF( X .GE. ZERO ) THEN
+            Z = X
+            W = Y
+         ELSE
+            Z = -X
+            W = -Y
+         END IF
+      ELSE IF( ABS(X) .LT. THRESH ) THEN
+         Z = -SIGMA*SIGMA
+         W = ZERO
+      ELSE
+         IF( X .GE. ZERO ) THEN
+            S = ONE
+         ELSE
+            S = NEGONE
+         END IF
+         Z = S * (ABS(X)-SIGMA) * (S+SIGMA/X)
+         W = S * Y
+      END IF
+*
+*     Generate the rotation.
+*     CALL DLARTGP( Z, W, CS, SN, R ) might seem more natural;
+*     reordering the arguments ensures that if Z = 0 then the rotation
+*     is by PI/2.
+*
+      CALL DLARTGP( W, Z, SN, CS, R )
+*
+      RETURN
+*
+*     End DLARTGS
+*
+      END
+
---
+++ lapack/trunk/SRC/zlapmr.f
@@ -0,0 +1,143 @@
+      SUBROUTINE ZLAPMR( FORWRD, M, N, X, LDX, K )
+      IMPLICIT NONE
+*
+*     Originally ZLAPMT
+*  -- LAPACK auxiliary routine (version 3.2) --
+*  -- LAPACK is a software package provided by Univ. of Tennessee,    --
+*  -- Univ. of California Berkeley, Univ. of Colorado Denver and NAG Ltd..--
+*     November 2006
+*
+*     Adapted to ZLAPMR by Brian Sutton
+*     July 2010
+*
+*     .. Scalar Arguments ..
+      LOGICAL            FORWRD
+      INTEGER            LDX, M, N
+*     ..
+*     .. Array Arguments ..
+      INTEGER            K( * )
+      COMPLEX*16         X( LDX, * )
+*     ..
+*
+*  Purpose
+*  =======
+*
+*  ZLAPMR rearranges the rows of the M by N matrix X as specified
+*  by the permutation K(1),K(2),...,K(M) of the integers 1,...,M.
+*  If FORWRD = .TRUE.,  forward permutation:
+*
+*       X(K(I),*) is moved X(I,*) for I = 1,2,...,M.
+*
+*  If FORWRD = .FALSE., backward permutation:
+*
+*       X(I,*) is moved to X(K(I),*) for I = 1,2,...,M.
+*
+*  Arguments
+*  =========
+*
+*  FORWRD  (input) LOGICAL
+*          = .TRUE., forward permutation
+*          = .FALSE., backward permutation
+*
+*  M       (input) INTEGER
+*          The number of rows of the matrix X. M >= 0.
+*
+*  N       (input) INTEGER
+*          The number of columns of the matrix X. N >= 0.
+*
+*  X       (input/output) COMPLEX*16 array, dimension (LDX,N)
+*          On entry, the M by N matrix X.
+*          On exit, X contains the permuted matrix X.
+*
+*  LDX     (input) INTEGER
+*          The leading dimension of the array X, LDX >= MAX(1,M).
+*
+*  K       (input/output) INTEGER array, dimension (M)
+*          On entry, K contains the permutation vector. K is used as
+*          internal workspace, but reset to its original value on
+*          output.
+*
+*  =====================================================================
+*
+*     .. Local Scalars ..
+      INTEGER            I, IN, J, JJ
+      COMPLEX*16         TEMP
+*     ..
+*     .. Executable Statements ..
+*
+      IF( M.LE.1 )
+     $   RETURN
+*
+      DO 10 I = 1, M
+         K( I ) = -K( I )
+   10 CONTINUE
+*
+      IF( FORWRD ) THEN
+*
+*        Forward permutation
+*
+         DO 50 I = 1, M
+*
+            IF( K( I ).GT.0 )
+     $         GO TO 40
+*
+            J = I
+            K( J ) = -K( J )
+            IN = K( J )
+*
+   20       CONTINUE
+            IF( K( IN ).GT.0 )
+     $         GO TO 40
+*
+            DO 30 JJ = 1, N
+               TEMP = X( J, JJ )
+               X( J, JJ ) = X( IN, JJ )
+               X( IN, JJ ) = TEMP
+   30       CONTINUE
+*
+            K( IN ) = -K( IN )
+            J = IN
+            IN = K( IN )
+            GO TO 20
+*
+   40       CONTINUE
+*
+   50    CONTINUE
+*
+      ELSE
+*
+*        Backward permutation
+*
+         DO 90 I = 1, M
+*
+            IF( K( I ).GT.0 )
+     $         GO TO 80
+*
+            K( I ) = -K( I )
+            J = K( I )
+   60       CONTINUE
+            IF( J.EQ.I )
+     $         GO TO 80
+*
+            DO 70 JJ = 1, N
+               TEMP = X( I, JJ )
+               X( I, JJ ) = X( J, JJ )
+               X( J, JJ ) = TEMP
+   70       CONTINUE
+*
+            K( J ) = -K( J )
+            J = K( J )
+            GO TO 60
+*
+   80       CONTINUE
+*
+   90    CONTINUE
+*
+      END IF
+*
+      RETURN
+*
+*     End of ZLAPMT
+*
+      END
+
