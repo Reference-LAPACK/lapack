1767365dbf4c05eb481a920a5c59beb20cbe9ba5
julie <julielangou@users.noreply.github.com>
2011-03-16 22:52:01 +0000

    Remove GO TO STATEMENT in DROTMG/DROTM and SROTMG/SROTM.
    Incorporate OLD Testings from ACM Collected algorithms : algorithm 539 
    for:
       - DROTMG/SROTMG
       - DROTM/SROTM
       - DSDOT / SDSDOT
    
    All BLAS routines are now tested.
    Now up to us to change the BLAS but at least this is consistent.

--- BLAS/SRC/srotm.f
+++ BLAS/SRC/srotm.f
@@ -12,7 +12,7 @@
 *     APPLY THE MODIFIED GIVENS TRANSFORMATION, H, TO THE 2 BY N MATRIX
 *
 *     (SX**T) , WHERE **T INDICATES TRANSPOSE. THE ELEMENTS OF SX ARE IN
-*     (DX**T)
+*     (SX**T)
 *
 *     SX(LX+I*INCX), I = 0 TO N-1, WHERE LX = 1 IF INCX .GE. 0, ELSE
 *     LX = (-INCX)*N, AND SIMILARLY FOR SY USING USING LY AND INCY.
@@ -62,99 +62,82 @@
 *     ..
 *
       SFLAG = SPARAM(1)
-      IF (N.LE.0 .OR. (SFLAG+TWO.EQ.ZERO)) GO TO 140
-      IF (.NOT. (INCX.EQ.INCY.AND.INCX.GT.0)) GO TO 70
+      IF (N.LE.0 .OR. (SFLAG+TWO.EQ.ZERO)) RETURN
+      IF (INCX.EQ.INCY.AND.INCX.GT.0) THEN
 *
-      NSTEPS = N*INCX
-      IF (SFLAG.LT.ZERO) THEN
-         GO TO 50
-      ELSE IF (SFLAG.EQ.ZERO) THEN
-         GO TO 10
+         NSTEPS = N*INCX
+         IF (SFLAG.LT.ZERO) THEN
+            SH11 = SPARAM(2)
+            SH12 = SPARAM(4)
+            SH21 = SPARAM(3)
+            SH22 = SPARAM(5)
+            DO I = 1,NSTEPS,INCX
+               W = SX(I)
+               Z = SY(I)
+               SX(I) = W*SH11 + Z*SH12
+               SY(I) = W*SH21 + Z*SH22
+            END DO
+         ELSE IF (SFLAG.EQ.ZERO) THEN
+            SH12 = SPARAM(4)
+            SH21 = SPARAM(3)
+            DO I = 1,NSTEPS,INCX
+               W = SX(I)
+               Z = SY(I)
+               SX(I) = W + Z*SH12
+               SY(I) = W*SH21 + Z
+            END DO
+         ELSE
+            SH11 = SPARAM(2)
+            SH22 = SPARAM(5)
+            DO I = 1,NSTEPS,INCX
+               W = SX(I)
+               Z = SY(I)
+               SX(I) = W*SH11 + Z
+               SY(I) = -W + SH22*Z
+            END DO
+         END IF
       ELSE
-         GO TO 30
-      END IF
-   10 CONTINUE
-      SH12 = SPARAM(4)
-      SH21 = SPARAM(3)
-      DO 20 I = 1,NSTEPS,INCX
-          W = SX(I)
-          Z = SY(I)
-          SX(I) = W + Z*SH12
-          SY(I) = W*SH21 + Z
-   20 CONTINUE
-      GO TO 140
-   30 CONTINUE
-      SH11 = SPARAM(2)
-      SH22 = SPARAM(5)
-      DO 40 I = 1,NSTEPS,INCX
-          W = SX(I)
-          Z = SY(I)
-          SX(I) = W*SH11 + Z
-          SY(I) = -W + SH22*Z
-   40 CONTINUE
-      GO TO 140
-   50 CONTINUE
-      SH11 = SPARAM(2)
-      SH12 = SPARAM(4)
-      SH21 = SPARAM(3)
-      SH22 = SPARAM(5)
-      DO 60 I = 1,NSTEPS,INCX
-          W = SX(I)
-          Z = SY(I)
-          SX(I) = W*SH11 + Z*SH12
-          SY(I) = W*SH21 + Z*SH22
-   60 CONTINUE
-      GO TO 140
-   70 CONTINUE
-      KX = 1
-      KY = 1
-      IF (INCX.LT.0) KX = 1 + (1-N)*INCX
-      IF (INCY.LT.0) KY = 1 + (1-N)*INCY
+         KX = 1
+         KY = 1
+         IF (INCX.LT.0) KX = 1 + (1-N)*INCX
+         IF (INCY.LT.0) KY = 1 + (1-N)*INCY
 *
-      IF (SFLAG.LT.ZERO) THEN
-         GO TO 120
-      ELSE IF (SFLAG.EQ.ZERO) THEN
-         GO TO 80
-      ELSE
-         GO TO 100
+         IF (SFLAG.LT.ZERO) THEN
+            SH11 = SPARAM(2)
+            SH12 = SPARAM(4)
+            SH21 = SPARAM(3)
+            SH22 = SPARAM(5)
+            DO I = 1,N
+               W = SX(KX)
+               Z = SY(KY)
+               SX(KX) = W*SH11 + Z*SH12
+               SY(KY) = W*SH21 + Z*SH22
+               KX = KX + INCX
+               KY = KY + INCY
+            END DO
+         ELSE IF (SFLAG.EQ.ZERO) THEN
+            SH12 = SPARAM(4)
+            SH21 = SPARAM(3)
+            DO I = 1,N
+               W = SX(KX)
+               Z = SY(KY)
+               SX(KX) = W + Z*SH12
+               SY(KY) = W*SH21 + Z
+               KX = KX + INCX
+               KY = KY + INCY
+            END DO
+         ELSE
+             SH11 = SPARAM(2)
+             SH22 = SPARAM(5)
+             DO I = 1,N
+                W = SX(KX)
+                Z = SY(KY)
+                SX(KX) = W*SH11 + Z
+                SY(KY) = -W + SH22*Z
+                KX = KX + INCX
+                KY = KY + INCY
+            END DO
+         END IF
       END IF
-   80 CONTINUE
-      SH12 = SPARAM(4)
-      SH21 = SPARAM(3)
-      DO 90 I = 1,N
-          W = SX(KX)
-          Z = SY(KY)
-          SX(KX) = W + Z*SH12
-          SY(KY) = W*SH21 + Z
-          KX = KX + INCX
-          KY = KY + INCY
-   90 CONTINUE
-      GO TO 140
-  100 CONTINUE
-      SH11 = SPARAM(2)
-      SH22 = SPARAM(5)
-      DO 110 I = 1,N
-          W = SX(KX)
-          Z = SY(KY)
-          SX(KX) = W*SH11 + Z
-          SY(KY) = -W + SH22*Z
-          KX = KX + INCX
-          KY = KY + INCY
-  110 CONTINUE
-      GO TO 140
-  120 CONTINUE
-      SH11 = SPARAM(2)
-      SH12 = SPARAM(4)
-      SH21 = SPARAM(3)
-      SH22 = SPARAM(5)
-      DO 130 I = 1,N
-          W = SX(KX)
-          Z = SY(KY)
-          SX(KX) = W*SH11 + Z*SH12
-          SY(KY) = W*SH21 + Z*SH22
-          KX = KX + INCX
-          KY = KY + INCY
-  130 CONTINUE
-  140 CONTINUE
       RETURN
       END
