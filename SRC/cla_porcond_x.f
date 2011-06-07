f82b1646d39a9d016fcaf90462bd42ebd4b86944
langou <langou@users.noreply.github.com>
2011-06-07 09:11:05 +0000

    For Yana Gurieva (Intel), see forum post 2508 on Tue Jun 7th, 2011.
    Typo in the description of cla_porcond_c and cla_porcond_x.
    ( zla_porcond_c and zla_porcond_x are good, just the c versions have the typos.)

--- SRC/cla_porcond_x.f
+++ SRC/cla_porcond_x.f
@@ -45,7 +45,7 @@
 *
 *     AF      (input) COMPLEX array, dimension (LDAF,N)
 *     The triangular factor U or L from the Cholesky factorization
-*     A = U**T*U or A = L*L**T, as computed by CPOTRF.
+*     A = U**H*U or A = L*L**H, as computed by CPOTRF.
 *
 *     LDAF    (input) INTEGER
 *     The leading dimension of the array AF.  LDAF >= max(1,N).
