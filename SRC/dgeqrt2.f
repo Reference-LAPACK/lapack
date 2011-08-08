ccebfeaeba01e127c3e80ab8f52e4d243a62d33d
james <james@8a072113-8704-0410-8d35-dd094bca7971>
2011-08-08 21:59:06 +0000

    QRT (QR factorization with compact WY representation for Q) routines for general matrices

---
+++ SRC/dgeqrt2.f
@@ -0,0 +1,157 @@
+      SUBROUTINE DGEQRT2( M, N, A, LDA, T, LDT, INFO )
+*
+*  -- LAPACK routine (version 3.?) --
+*  -- LAPACK is a software package provided by Univ. of Tennessee,    --
+*  -- Univ. of California Berkeley, Univ. of Colorado Denver and NAG Ltd..--
+*  -- July 2011                                                      --
+*
+*     .. Scalar Arguments ..
+      INTEGER   INFO, LDA, LDT, M, N
+*     ..
+*     .. Array Arguments ..
+      DOUBLE PRECISION   A( LDA, * ), T( LDT, * )
+*     ..
+*
+*  Purpose
+*  =======
+*
+*  DGEQRT2 computes a QR factorization of a real M-by-N matrix A, 
+*  using the compact WY representation of Q. 
+*
+*  Arguments
+*  =========
+*
+*  M       (input) INTEGER
+*          The number of rows of the matrix A.  M >= N.
+*
+*  N       (input) INTEGER
+*          The number of columns of the matrix A.  N >= 0.
+*
+*  A       (input/output) DOUBLE PRECISION array, dimension (LDA,N)
+*          On entry, the real M-by-N matrix A.  On exit, the elements on and
+*          above the diagonal contain the N-by-N upper triangular matrix R; the
+*          elements below the diagonal are the columns of V.  See below for
+*          further details.
+*
+*  LDA     (input) INTEGER
+*          The leading dimension of the array A.  LDA >= max(1,M).
+*
+*  T       (output) DOUBLE PRECISION array, dimension (LDT,N)
+*          The N-by-N upper triangular factor of the block reflector.
+*          The elements on and above the diagonal contain the block
+*          reflector T; the elements below the diagonal are not used.
+*          See below for further details.
+*
+*  LDT     (intput) INTEGER
+*          The leading dimension of the array T.  LDT >= max(1,N).
+*
+*  INFO    (output) INTEGER
+*          = 0: successful exit
+*          < 0: if INFO = -i, the i-th argument had an illegal value
+*
+*  Further Details
+*  ===============
+*
+*  The matrix V stores the elementary reflectors H(i) in the i-th column
+*  below the diagonal. For example, if M=5 and N=3, the matrix V is
+*
+*               V = (  1       )
+*                   ( v1  1    )
+*                   ( v1 v2  1 )
+*                   ( v1 v2 v3 )
+*                   ( v1 v2 v3 )
+*
+*  where the vi's represent the vectors which define H(i), which are returned
+*  in the matrix A.  The 1's along the diagonal of V are not stored in A.  The
+*  block reflector H is then given by
+*
+*               H = I - V * T * V**T
+*
+*  where V**T is the transpose of V.
+*
+*  =====================================================================
+*
+*     .. Parameters ..
+      DOUBLE PRECISION  ONE, ZERO
+      PARAMETER( ONE = 1.0D+00, ZERO = 0.0D+00 )
+*     ..
+*     .. Local Scalars ..
+      INTEGER   I, K
+      DOUBLE PRECISION   AII, ALPHA
+*     ..
+*     .. External Subroutines ..
+      EXTERNAL  DLARFG, DGEMV, DGER, DTRMV, XERBLA
+*     ..
+*     .. Executable Statements ..
+*
+*     Test the input arguments
+*
+      INFO = 0
+      IF( M.LT.0 ) THEN
+         INFO = -1
+      ELSE IF( N.LT.0 ) THEN
+         INFO = -2
+      ELSE IF( LDA.LT.MAX( 1, M ) ) THEN
+         INFO = -4
+      ELSE IF( LDT.LT.MAX( 1, N ) ) THEN
+         INFO = -6
+      END IF
+      IF( INFO.NE.0 ) THEN
+         CALL XERBLA( 'DGEQRT2', -INFO )
+         RETURN
+      END IF
+*      
+      K = MIN( M, N )
+*
+      DO I = 1, K
+*
+*        Generate elem. refl. H(i) to annihilate A(i+1:m,i), tau(I) -> T(I,1)
+*
+         CALL DLARFG( M-I+1, A( I, I ), A( MIN( I+1, M ), I ), 1,
+     $                T( I, 1 ) )
+         IF( I.LT.N ) THEN
+*
+*           Apply H(i) to A(I:M,I+1:N) from the left
+*
+            AII = A( I, I )
+            A( I, I ) = ONE
+*
+*           W(1:N-I) := A(I:M,I+1:N)^H * A(I:M,I) [W = T(:,N)]
+*
+            CALL DGEMV( 'T',M-I+1, N-I, ONE, A( I, I+1 ), LDA, 
+     $                  A( I, I ), 1, ZERO, T( 1, N ), 1 )
+*
+*           A(I:M,I+1:N) = A(I:m,I+1:N) + alpha*A(I:M,I)*W(1:N-1)^H
+*
+            ALPHA = -(T( I, 1 ))
+            CALL DGER( M-I+1, N-I, ALPHA, A( I, I ), 1, 
+     $           T( 1, N ), 1, A( I, I+1 ), LDA )
+            A( I, I ) = AII
+         END IF
+      END DO
+*
+      DO I = 2, N
+         AII = A( I, I )
+         A( I, I ) = ONE
+*
+*        T(1:I-1,I) := alpha * A(I:M,1:I-1)**T * A(I:M,I)
+*
+         ALPHA = -T( I, 1 )
+         CALL DGEMV( 'T', M-I+1, I-1, ALPHA, A( I, 1 ), LDA, 
+     $               A( I, I ), 1, ZERO, T( 1, I ), 1 )
+         A( I, I ) = AII
+*
+*        T(1:I-1,I) := T(1:I-1,1:I-1) * T(1:I-1,I)
+*
+         CALL DTRMV( 'U', 'N', 'N', I-1, T, LDT, T( 1, I ), 1 )
+*
+*           T(I,I) = tau(I)
+*
+            T( I, I ) = T( I, 1 )
+            T( I, 1) = ZERO
+      END DO
+   
+*
+*     End of DGEQRT2
+*
+      END
---
+++ SRC/sgeqrt.f
@@ -0,0 +1,143 @@
+      SUBROUTINE SGEQRT( M, N, NB, A, LDA, T, LDT, WORK, INFO )
+      IMPLICIT NONE
+*
+*  -- LAPACK routine (version 3.?) --
+*  -- LAPACK is a software package provided by Univ. of Tennessee,    --
+*  -- Univ. of California Berkeley, Univ. of Colorado Denver and NAG Ltd..--
+*  -- July 2011 --
+*
+*     .. Scalar Arguments ..
+      INTEGER INFO, LDA, LDT, M, N, NB
+*     ..
+*     .. Array Arguments ..
+      REAL A( LDA, * ), T( LDT, * ), WORK( * )
+*     ..
+*
+*  Purpose
+*  =======
+*
+*  SGEQRT computes a blocked QR factorization of a real M-by-N matrix A
+*  using the compact WY representation of Q.  
+*
+*  Arguments
+*  =========
+*
+*  M       (input) INTEGER
+*          The number of rows of the matrix A.  M >= 0.
+*
+*  N       (input) INTEGER
+*          The number of columns of the matrix A.  N >= 0.
+*
+*  NB      (input) INTEGER
+*          The block size to be used in the blocked QR.  MIN(M,N) >= NB >= 1.
+*
+*  A       (input/output) REAL array, dimension (LDA,N)
+*          On entry, the M-by-N matrix A.
+*          On exit, the elements on and above the diagonal of the array
+*          contain the min(M,N)-by-N upper trapezoidal matrix R (R is
+*          upper triangular if M >= N); the elements below the diagonal
+*          are the columns of V.  See below for further details.
+*
+*  LDA     (input) INTEGER
+*          The leading dimension of the array A.  LDA >= max(1,M).
+*
+*  T       (output) REAL array, dimension (LDT,MIN(M,N))
+*          The upper triangular block reflectors stored in compact form
+*          as a sequence of upper triangular blocks.  See below
+*          for further details.
+*          
+*  LDT     (input) INTEGER
+*          The leading dimension of the array T.  LDT >= NB.
+*
+*  WORK    (workspace) REAL array, dimension (NB*N)
+*
+*  INFO    (output) INTEGER
+*          = 0:  successful exit
+*          < 0:  if INFO = -i, the i-th argument had an illegal value
+*
+*  Further Details
+*  ===============
+*  
+*  The matrix V stores the elementary reflectors H(i) in the i-th column
+*  below the diagonal. For example, if M=5 and N=3, the matrix V is
+*
+*               V = (  1       )
+*                   ( v1  1    )
+*                   ( v1 v2  1 )
+*                   ( v1 v2 v3 )
+*                   ( v1 v2 v3 )
+*
+*  where the vi's represent the vectors which define H(i), which are returned
+*  in the matrix A.  The 1's along the diagonal of V are not stored in A.
+*
+*  Let K=MIN(M,N).  The number of blocks is B = ceiling(K/NB), where each
+*  block is of order NB except for the last block, which is of order 
+*  IB = K - (B-1)*NB.  For each of the B blocks, a upper triangular block
+*  reflector factor is computed: T1, T2, ..., TB.  The NB-by-NB (and IB-by-IB 
+*  for the last block) T's are stored in the NB-by-N matrix T as
+*
+*               T = (T1 T2 ... TB).
+*
+* =====================================================================
+*
+*     ..
+*     .. Local Scalars ..
+      INTEGER    I, IB, IINFO, K
+      LOGICAL    USE_RECURSIVE_QR
+      PARAMETER( USE_RECURSIVE_QR=.TRUE. )
+*     ..
+*     .. External Subroutines ..
+      EXTERNAL   SGEQRT2, SGEQRT3, SLARFB, XERBLA
+*     ..
+*     .. Executable Statements ..
+*
+*     Test the input arguments
+*
+      INFO = 0
+      IF( M.LT.0 ) THEN
+         INFO = -1
+      ELSE IF( N.LT.0 ) THEN
+         INFO = -2
+      ELSE IF( NB.LT.1 .OR. NB.GT.MIN(M,N) ) THEN
+         INFO = -3
+      ELSE IF( LDA.LT.MAX( 1, M ) ) THEN
+         INFO = -5
+      ELSE IF( LDT.LT.NB ) THEN
+         INFO = -7
+      END IF
+      IF( INFO.NE.0 ) THEN
+         CALL XERBLA( 'SGEQRT', -INFO )
+         RETURN
+      END IF
+*
+*     Quick return if possible
+*
+      K = MIN( M, N )
+      IF( K.EQ.0 ) RETURN
+*
+*     Blocked loop of length K
+*
+      DO I = 1, K,  NB
+         IB = MIN( K-I+1, NB )
+*     
+*     Compute the QR factorization of the current block A(I:M,I:I+IB-1)
+*
+         IF( USE_RECURSIVE_QR ) THEN
+            CALL SGEQRT3( M-I+1, IB, A(I,I), LDA, T(1,I), LDT, IINFO )
+         ELSE
+            CALL SGEQRT2( M-I+1, IB, A(I,I), LDA, T(1,I), LDT, IINFO )
+         END IF
+         IF( I+IB.LE.N ) THEN
+*
+*     Update by applying H**T to A(I:M,I+IB:N) from the left
+*
+            CALL SLARFB( 'L', 'T', 'F', 'C', M-I+1, N-I-IB+1, IB,
+     $                   A( I, I ), LDA, T( 1, I ), LDT, 
+     $                   A( I, I+IB ), LDA, WORK , N-I-IB+1 )
+         END IF
+      END DO
+      RETURN
+*     
+*     End of SGEQRT
+*
+      END
