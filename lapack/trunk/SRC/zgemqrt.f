551d5adae4a8e0314bfc22d271fc1297ed0e2d01
james <james@8a072113-8704-0410-8d35-dd094bca7971>
2011-08-08 21:59:06 +0000

    QRT (QR factorization with compact WY representation for Q) routines for general matrices

---
+++ lapack/trunk/SRC/zgemqrt.f
@@ -0,0 +1,202 @@
+      SUBROUTINE ZGEMQRT( SIDE, TRANS, M, N, K, NB, V, LDV, T, LDT, 
+     $                   C, LDC, WORK, INFO )
+      IMPLICIT NONE
+*
+*  -- LAPACK routine (version 3.?) --
+*  -- LAPACK is a software package provided by Univ. of Tennessee, --
+*  -- Univ. of California Berkeley, Univ. of Colorado Denver and NAG Ltd. --
+*  -- July 2011 --
+*
+*     .. Scalar Arguments ..
+      CHARACTER SIDE, TRANS
+      INTEGER   INFO, K, LDV, LDC, M, N, NB, LDT
+*     ..
+*     .. Array Arguments ..
+      COMPLEX*16   V( LDV, * ), C( LDC, * ), T( LDT, * ), WORK( * )
+*     ..
+*
+*  Purpose
+*  =======
+*
+*  ZGEMQRT overwrites the general complex M-by-N matrix C with
+*
+*                  SIDE = 'L'     SIDE = 'R'
+*  TRANS = 'N':      Q C            C Q
+*  TRANS = 'C':    Q**H C            C Q**H
+*
+*  where Q is a complex orthogonal matrix defined as the product of K
+*  elementary reflectors:
+*
+*        Q = H(1) H(2) . . . H(K) = I - V T V**H
+*
+*  generated using the compact WY representation as returned by ZGEQRT. 
+*
+*  Q is of order M if SIDE = 'L' and of order N  if SIDE = 'R'.
+*
+*  Arguments
+*  =========
+*
+*  SIDE    (input) CHARACTER*1
+*          = 'L': apply Q or Q**H from the Left;
+*          = 'R': apply Q or Q**H from the Right.
+*
+*  TRANS   (input) CHARACTER*1
+*          = 'N':  No transpose, apply Q;
+*          = 'C':  Transpose, apply Q**H.
+*
+*  M       (input) INTEGER
+*          The number of rows of the matrix C. M >= 0.
+*
+*  N       (input) INTEGER
+*          The number of columns of the matrix C. N >= 0.
+*
+*  K       (input) INTEGER
+*          The number of elementary reflectors whose product defines
+*          the matrix Q.
+*          If SIDE = 'L', M >= K >= 0;
+*          if SIDE = 'R', N >= K >= 0.
+*
+*  NB      (input) INTEGER
+*          The block size used for the storage of T.  K >= NB >= 1.
+*          This must be the same value of NB used to generate T
+*          in CGEQRT.
+*
+*  V       (input) COMPLEX*16 array, dimension (LDV,K)
+*          The i-th column must contain the vector which defines the
+*          elementary reflector H(i), for i = 1,2,...,k, as returned by
+*          CGEQRT in the first K columns of its array argument A.
+*
+*  LDV     (input) INTEGER
+*          The leading dimension of the array V.
+*          If SIDE = 'L', LDA >= max(1,M);
+*          if SIDE = 'R', LDA >= max(1,N).
+*
+*  T       (input) COMPLEX*16 array, dimension (LDT,K)
+*          The upper triangular factors of the block reflectors
+*          as returned by CGEQRT, stored as a NB-by-N matrix.
+*
+*  LDT     (input) INTEGER
+*          The leading dimension of the array T.  LDT >= NB.
+*
+*  C       (input/output) COMPLEX*16 array, dimension (LDC,N)
+*          On entry, the M-by-N matrix C.
+*          On exit, C is overwritten by Q C, Q**H C, C Q**H or C Q.
+*
+*  LDC     (input) INTEGER
+*          The leading dimension of the array C. LDC >= max(1,M).
+*
+*  WORK    (workspace/output) COMPLEX*16 array.  The dimension of WORK is
+*           N*NB if SIDE = 'L', or  M*NB if SIDE = 'R'.
+*
+*  INFO    (output) INTEGER
+*          = 0:  successful exit
+*          < 0:  if INFO = -i, the i-th argument had an illegal value
+*
+*  =====================================================================
+*
+*     ..
+*     .. Local Scalars ..
+      LOGICAL            LEFT, RIGHT, TRAN, NOTRAN
+      INTEGER            I, IB, LDWORK, KF, Q
+*     ..
+*     .. External Functions ..
+      LOGICAL            LSAME
+      EXTERNAL           LSAME
+*     ..
+*     .. External Subroutines ..
+      EXTERNAL           XERBLA, ZLARFB
+*     ..
+*     .. Intrinsic Functions ..
+      INTRINSIC          MAX, MIN
+*     ..
+*     .. Executable Statements ..
+*
+*     .. Test the input arguments ..
+*
+      INFO   = 0
+      LEFT   = LSAME( SIDE,  'L' )
+      RIGHT  = LSAME( SIDE,  'R' )
+      TRAN   = LSAME( TRANS, 'C' )
+      NOTRAN = LSAME( TRANS, 'N' )
+*      
+      IF( LEFT ) THEN
+         LDWORK = MAX( 1, N )
+         Q = M
+      ELSE IF ( RIGHT ) THEN
+         LDWORK = MAX( 1, M )
+         Q = N
+      END IF
+      IF( .NOT.LEFT .AND. .NOT.RIGHT ) THEN
+         INFO = -1
+      ELSE IF( .NOT.TRAN .AND. .NOT.NOTRAN ) THEN
+         INFO = -2
+      ELSE IF( M.LT.0 ) THEN
+         INFO = -3
+      ELSE IF( N.LT.0 ) THEN
+         INFO = -4
+      ELSE IF( K.LT.0 .OR. K.GT.Q ) THEN
+         INFO = -5
+      ELSE IF( NB.LT.1 .OR. NB.GT.K ) THEN
+         INFO = -6
+      ELSE IF( LDV.LT.MAX( 1, Q ) ) THEN
+         INFO = -8
+      ELSE IF( LDT.LT.NB ) THEN
+         INFO = -10
+      ELSE IF( LDC.LT.MAX( 1, M ) ) THEN
+         INFO = -12
+      END IF
+*
+      IF( INFO.NE.0 ) THEN
+         CALL XERBLA( 'ZGEMQRT', -INFO )
+         RETURN
+      END IF
+*
+*     .. Quick return if possible ..
+*
+      IF( M.EQ.0 .OR. N.EQ.0 .OR. K.EQ.0 ) RETURN
+*
+      IF( LEFT .AND. TRAN ) THEN
+*
+         DO I = 1, K, NB
+            IB = MIN( NB, K-I+1 )
+            CALL ZLARFB( 'L', 'C', 'F', 'C', M-I+1, N, IB, 
+     $                   V( I, I ), LDV, T( 1, I ), LDT, 
+     $                   C( I, 1 ), LDC, WORK, LDWORK )
+         END DO
+*         
+      ELSE IF( RIGHT .AND. NOTRAN ) THEN
+*
+         DO I = 1, K, NB
+            IB = MIN( NB, K-I+1 )
+            CALL ZLARFB( 'R', 'N', 'F', 'C', M, N-I+1, IB, 
+     $                   V( I, I ), LDV, T( 1, I ), LDT, 
+     $                   C( 1, I ), LDC, WORK, LDWORK )
+         END DO
+*
+      ELSE IF( LEFT .AND. NOTRAN ) THEN
+*
+         KF = ((K-1)/NB)*NB+1
+         DO I = KF, 1, -NB
+            IB = MIN( NB, K-I+1 )         
+            CALL ZLARFB( 'L', 'N', 'F', 'C', M-I+1, N, IB, 
+     $                   V( I, I ), LDV, T( 1, I ), LDT, 
+     $                   C( I, 1 ), LDC, WORK, LDWORK )
+         END DO
+*
+      ELSE IF( RIGHT .AND. TRAN ) THEN
+*
+         KF = ((K-1)/NB)*NB+1
+         DO I = KF, 1, -NB
+            IB = MIN( NB, K-I+1 )         
+            CALL ZLARFB( 'R', 'C', 'F', 'C', M, N-I+1, IB, 
+     $                   V( I, I ), LDV, T( 1, I ), LDT, 
+     $                   C( 1, I ), LDC, WORK, LDWORK )
+         END DO
+*
+      END IF
+*
+      RETURN
+*
+*     End of ZGEMQRT
+*
+      END
