2a41d09a428b2b1c77650a636ca7dffae6127b43
james <james@8a072113-8704-0410-8d35-dd094bca7971>
2011-12-08 23:55:05 +0000

    corrected documentation

--- SRC/ctzrzf.f
+++ SRC/ctzrzf.f
@@ -130,31 +130,22 @@
 *>
 *> \verbatim
 *>
-*>  The factorization is obtained by Householder's method.  The kth
-*>  transformation matrix, Z( k ), which is used to introduce zeros into
-*>  the ( m - k + 1 )th row of A, is given in the form
+*>  The N-by-N matrix Z can be computed by
 *>
-*>     Z( k ) = ( I     0   ),
-*>              ( 0  T( k ) )
+*>     Z =  Z(1)*Z(2)* ... *Z(M)
 *>
-*>  where
+*>  where each N-by-N Z(k) is given by
 *>
-*>     T( k ) = I - tau*u( k )*u( k )**H,   u( k ) = (   1    ),
-*>                                                 (   0    )
-*>                                                 ( z( k ) )
+*>     Z(k) = I - tau(k)*v(k)*v(k)**H
 *>
-*>  tau is a scalar and z( k ) is an ( n - m ) element vector.
-*>  tau and z( k ) are chosen to annihilate the elements of the kth row
-*>  of X.
+*>  with v(k) is the kth row vector of the M-by-N matrix
 *>
-*>  The scalar tau is returned in the kth element of TAU and the vector
-*>  u( k ) in the kth row of A, such that the elements of z( k ) are
-*>  in  a( k, m + 1 ), ..., a( k, n ). The elements of R are returned in
-*>  the upper triangular part of A.
+*>     V = ( I   A(:,M+1:N) )
 *>
-*>  Z is given by
+*>  I is the M-by-M identity matrix, A(:,M+1:N) 
+*>  is the output stored in A on exit from DTZRZF,
+*>  and tau(k) is the kth element of the array TAU.
 *>
-*>     Z =  Z( 1 ) * Z( 2 ) * ... * Z( m ).
 *> \endverbatim
 *>
 *  =====================================================================
