e5962daa5370c1c1bd0f061171e4def452e18a1d
lipshitz <lipshitz@8a072113-8704-0410-8d35-dd094bca7971>
2011-12-20 03:10:11 +0000

    In dqds, set shift to zero if it is insignificant compared to the cumulative shift, and set a d to zero if it is insignificant compared to the cumulative shift and the current shift is zero.  This should guarantee that it always finishes in a linear number of iterations, without hurting accuracy at all.

--- SRC/dlasq3.f
+++ SRC/dlasq3.f
@@ -331,15 +331,15 @@
 *
    70 CONTINUE
 *
-      CALL DLASQ5( I0, N0, Z, PP, TAU, DMIN, DMIN1, DMIN2, DN,
-     $             DN1, DN2, IEEE )
+      CALL DLASQ5( I0, N0, Z, PP, TAU, SIGMA, DMIN, DMIN1, DMIN2, DN,
+     $             DN1, DN2, IEEE, EPS )
 *
       NDIV = NDIV + ( N0-I0+2 )
       ITER = ITER + 1
 *
 *     Check status.
 *
-      IF( DMIN.GE.ZERO .AND. DMIN1.GT.ZERO ) THEN
+      IF( DMIN.GE.ZERO .AND. DMIN1.GE.ZERO ) THEN
 *
 *        Success.
 *
