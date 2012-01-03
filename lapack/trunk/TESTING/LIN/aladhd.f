594b792120893901221bc053ee72aefd4a6bcadc
igor175 <igor175@8a072113-8704-0410-8d35-dd094bca7971>
2012-01-03 23:27:40 +0000

    added 'SR' test path to aladhd.f for driver routines that correspond to symmetric indefinite matrices with rook pivoting

--- lapack/trunk/TESTING/LIN/aladhd.f
+++ lapack/trunk/TESTING/LIN/aladhd.f
@@ -47,10 +47,16 @@
 *>             _PP:  Symmetric or Hermitian positive definite packed
 *>             _PB:  Symmetric or Hermitian positive definite band
 *>             _PT:  Symmetric or Hermitian positive definite tridiagonal
-*>             _SY:  Symmetric indefinite
-*>             _SP:  Symmetric indefinite packed
-*>             _HE:  (complex) Hermitian indefinite
-*>             _HP:  (complex) Hermitian indefinite packed
+*>             _SY:  Symmetric indefinite,
+*>                     with partial (Bunch-Kaufman) pivoting
+*>             _SR:  Symmetric indefinite,
+*>                     with "rook" (bounded Bunch-Kaufman) pivoting
+*>             _SP:  Symmetric indefinite packed,
+*>                     with partial (Bunch-Kaufman) pivoting
+*>             _HE:  (complex) Hermitian indefinite,
+*>                     with partial (Bunch-Kaufman) pivoting
+*>             _HP:  (complex) Hermitian indefinite packed,
+*>                     with partial (Bunch-Kaufman) pivoting
 *>          The first character must be one of S, D, C, or Z (C or Z only
 *>          if complex).
 *> \endverbatim
@@ -223,7 +229,9 @@
       ELSE IF( LSAMEN( 2, P2, 'SY' ) .OR. LSAMEN( 2, P2, 'SP' ) ) THEN
 *
 *        SY: Symmetric indefinite full
+*            with partial (Bunch-Kaufman) pivoting algorithm
 *        SP: Symmetric indefinite packed
+*            with partial (Bunch-Kaufman) pivoting algorithm
 *
          IF( LSAME( C3, 'Y' ) ) THEN
             WRITE( IOUNIT, FMT = 9992 )PATH, 'Symmetric'
@@ -245,10 +253,32 @@
          WRITE( IOUNIT, FMT = 9976 )6
          WRITE( IOUNIT, FMT = '( '' Messages:'' )' )
 *
+      ELSE IF( LSAMEN( 2, P2, 'SR' )  ) THEN
+*
+*        SR: Symmetric indefinite full,
+*            with "rook" (bounded Bunch-Kaufman) pivoting algorithm
+*
+         WRITE( IOUNIT, FMT = 9992 )PATH, 'Symmetric'
+*
+         WRITE( IOUNIT, FMT = '( '' Matrix types:'' )' )
+         IF( SORD ) THEN
+            WRITE( IOUNIT, FMT = 9983 )
+         ELSE
+            WRITE( IOUNIT, FMT = 9982 )
+         END IF
+*
+         WRITE( IOUNIT, FMT = '( '' Test ratios:'' )' )
+         WRITE( IOUNIT, FMT = 9974 )1
+         WRITE( IOUNIT, FMT = 9980 )2
+         WRITE( IOUNIT, FMT = 9979 )3
+         WRITE( IOUNIT, FMT = '( '' Messages:'' )' )
+*
       ELSE IF( LSAMEN( 2, P2, 'HE' ) .OR. LSAMEN( 2, P2, 'HP' ) ) THEN
 *
 *        HE: Hermitian indefinite full
+*            with partial (Bunch-Kaufman) pivoting algorithm
 *        HP: Hermitian indefinite packed
+*            with partial (Bunch-Kaufman) pivoting algorithm
 *
          IF( LSAME( C3, 'E' ) ) THEN
             WRITE( IOUNIT, FMT = 9992 )PATH, 'Hermitian'
@@ -286,9 +316,16 @@
      $      ' positive definite band matrices' )
  9993 FORMAT( / 1X, A3, ' drivers:  ', A9,
      $      ' positive definite tridiagonal' )
- 9992 FORMAT( / 1X, A3, ' drivers:  ', A9, ' indefinite matrices' )
+ 9992 FORMAT( / 1X, A3, ' drivers:  ', A9, ' indefinite matrices',
+      $     ', rook (bounded Bunch-Kaufman) pivoting' )
  9991 FORMAT( / 1X, A3, ' drivers:  ', A9,
-     $      ' indefinite packed matrices' )
+     $      ' indefinite packed matrices',
+     $      ', partial (Bunch-Kaufman) pivoting' )
+ 9892 FORMAT( / 1X, A3, ' drivers:  ', A9, ' indefinite matrices',
+     $      ', rook (bounded Bunch-Kaufman) pivoting' )
+ 9891 FORMAT( / 1X, A3, ' drivers:  ', A9,
+            ' indefinite packed matrices',
+     $      ', rook (bounded Bunch-Kaufman) pivoting' )
  9990 FORMAT( / 1X, A3, ':  No header available' )
 *
 *     GE matrix types
