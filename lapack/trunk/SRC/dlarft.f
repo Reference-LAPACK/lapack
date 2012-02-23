15ed2333a9b5ca339d508eec8cfa446e1df25b44
james <james@8a072113-8704-0410-8d35-dd094bca7971>
2012-02-23 19:34:36 +0000

    modified so that V is [in] only instead of [in/out]

--- lapack/trunk/SRC/dlarft.f
+++ lapack/trunk/SRC/dlarft.f
@@ -86,7 +86,7 @@
 *>          elementary reflectors). K >= 1.
 *> \endverbatim
 *>
-*> \param[in,out] V
+*> \param[in] V
 *> \verbatim
 *>          V is DOUBLE PRECISION array, dimension
 *>                               (LDV,K) if STOREV = 'C'
@@ -141,9 +141,7 @@
 *>
 *>  The shape of the matrix V and the storage of the vectors which define
 *>  the H(i) is best illustrated by the following example with n = 5 and
-*>  k = 3. The elements equal to 1 are not stored; the corresponding
-*>  array elements are modified but restored on exit. The rest of the
-*>  array is not used.
+*>  k = 3. The elements equal to 1 are not stored.
 *>
 *>  DIRECT = 'F' and STOREV = 'C':         DIRECT = 'F' and STOREV = 'R':
 *>
@@ -186,7 +184,6 @@
 *     ..
 *     .. Local Scalars ..
       INTEGER            I, J, PREVLASTV, LASTV
-      DOUBLE PRECISION   VII
 *     ..
 *     .. External Subroutines ..
       EXTERNAL           DGEMV, DTRMV
@@ -204,47 +201,50 @@
 *
       IF( LSAME( DIRECT, 'F' ) ) THEN
          PREVLASTV = N
-         DO 20 I = 1, K
+         DO I = 1, K
             PREVLASTV = MAX( I, PREVLASTV )
             IF( TAU( I ).EQ.ZERO ) THEN
 *
 *              H(i)  =  I
 *
-               DO 10 J = 1, I
+               DO J = 1, I
                   T( J, I ) = ZERO
-   10          CONTINUE
+               END DO
             ELSE
 *
 *              general case
 *
-               VII = V( I, I )
-               V( I, I ) = ONE
                IF( LSAME( STOREV, 'C' ) ) THEN
-!                 Skip any trailing zeros.
+*                 Skip any trailing zeros.
                   DO LASTV = N, I+1, -1
                      IF( V( LASTV, I ).NE.ZERO ) EXIT
                   END DO
+                  DO J = 1, I-1
+                     T( J, I ) = -TAU( I ) * V( I , J )
+                  END DO   
                   J = MIN( LASTV, PREVLASTV )
 *
 *                 T(1:i-1,i) := - tau(i) * V(i:j,1:i-1)**T * V(i:j,i)
 *
-                  CALL DGEMV( 'Transpose', J-I+1, I-1, -TAU( I ),
-     $                        V( I, 1 ), LDV, V( I, I ), 1, ZERO,
+                  CALL DGEMV( 'Transpose', J-I, I-1, -TAU( I ), 
+     $                        V( I+1, 1 ), LDV, V( I+1, I ), 1, ONE, 
      $                        T( 1, I ), 1 )
                ELSE
-!                 Skip any trailing zeros.
+*                 Skip any trailing zeros.
                   DO LASTV = N, I+1, -1
                      IF( V( I, LASTV ).NE.ZERO ) EXIT
                   END DO
+                  DO J = 1, I-1
+                     T( J, I ) = -TAU( I ) * V( J , I )
+                  END DO   
                   J = MIN( LASTV, PREVLASTV )
 *
 *                 T(1:i-1,i) := - tau(i) * V(1:i-1,i:j) * V(i,i:j)**T
 *
-                  CALL DGEMV( 'No transpose', I-1, J-I+1, -TAU( I ),
-     $                        V( 1, I ), LDV, V( I, I ), LDV, ZERO,
+                  CALL DGEMV( 'No transpose', I-1, J-I, -TAU( I ),
+     $                        V( 1, I+1 ), LDV, V( I, I+1 ), LDV, ONE,
      $                        T( 1, I ), 1 )
                END IF
-               V( I, I ) = VII
 *
 *              T(1:i-1,i) := T(1:i-1,1:i-1) * T(1:i-1,i)
 *
@@ -257,54 +257,52 @@
                   PREVLASTV = LASTV
                END IF
             END IF
-   20    CONTINUE
+         END DO
       ELSE
          PREVLASTV = 1
-         DO 40 I = K, 1, -1
+         DO I = K, 1, -1
             IF( TAU( I ).EQ.ZERO ) THEN
 *
 *              H(i)  =  I
 *
-               DO 30 J = I, K
+               DO J = I, K
                   T( J, I ) = ZERO
-   30          CONTINUE
+               END DO
             ELSE
 *
 *              general case
 *
                IF( I.LT.K ) THEN
                   IF( LSAME( STOREV, 'C' ) ) THEN
-                     VII = V( N-K+I, I )
-                     V( N-K+I, I ) = ONE
-!                    Skip any leading zeros.
+*                    Skip any leading zeros.
                      DO LASTV = 1, I-1
                         IF( V( LASTV, I ).NE.ZERO ) EXIT
                      END DO
+                     DO J = I+1, K
+                        T( J, I ) = -TAU( I ) * V( N-K+I , J )
+                     END DO   
                      J = MAX( LASTV, PREVLASTV )
 *
-*                    T(i+1:k,i) :=
-*                            - tau(i) * V(j:n-k+i,i+1:k)**T * V(j:n-k+i,i)
+*                    T(i+1:k,i) = -tau(i) * V(j:n-k+i,i+1:k)**T * V(j:n-k+i,i)
 *
-                     CALL DGEMV( 'Transpose', N-K+I-J+1, K-I, -TAU( I ),
-     $                           V( J, I+1 ), LDV, V( J, I ), 1, ZERO,
+                     CALL DGEMV( 'Transpose', N-K+I-J, K-I, -TAU( I ),
+     $                           V( J, I+1 ), LDV, V( J, I ), 1, ONE,
      $                           T( I+1, I ), 1 )
-                     V( N-K+I, I ) = VII
                   ELSE
-                     VII = V( I, N-K+I )
-                     V( I, N-K+I ) = ONE
-!                    Skip any leading zeros.
+*                    Skip any leading zeros.
                      DO LASTV = 1, I-1
                         IF( V( I, LASTV ).NE.ZERO ) EXIT
                      END DO
+                     DO J = I+1, K
+                        T( J, I ) = -TAU( I ) * V( J, N-K+I )
+                     END DO   
                      J = MAX( LASTV, PREVLASTV )
 *
-*                    T(i+1:k,i) :=
-*                            - tau(i) * V(i+1:k,j:n-k+i) * V(i,j:n-k+i)**T
+*                    T(i+1:k,i) = -tau(i) * V(i+1:k,j:n-k+i) * V(i,j:n-k+i)**T
 *
-                     CALL DGEMV( 'No transpose', K-I, N-K+I-J+1,
+                     CALL DGEMV( 'No transpose', K-I, N-K+I-J,
      $                    -TAU( I ), V( I+1, J ), LDV, V( I, J ), LDV,
-     $                    ZERO, T( I+1, I ), 1 )
-                     V( I, N-K+I ) = VII
+     $                    ONE, T( I+1, I ), 1 )
                   END IF
 *
 *                 T(i+1:k,i) := T(i+1:k,i+1:k) * T(i+1:k,i)
@@ -319,7 +317,7 @@
                END IF
                T( I, I ) = TAU( I )
             END IF
-   40    CONTINUE
+         END DO
       END IF
       RETURN
 *
