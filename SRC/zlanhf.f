c254495b4de7667073f941bdfe89f8325c66687b
james <james@8a072113-8704-0410-8d35-dd094bca7971>
2012-04-11 01:50:07 +0000

    added code to handle N=1 case to fix divide by zero problem that occurs when the single (diagonal) element is negative
    see: http://icl.cs.utk.edu/lapack-forum/archives/lapack/msg01195.html

--- SRC/zlanhf.f
+++ SRC/zlanhf.f
@@ -286,6 +286,9 @@
       IF( N.EQ.0 ) THEN
          ZLANHF = ZERO
          RETURN
+      ELSE IF( N.EQ.1 ) THEN
+         ZLANHF = ABS( A(0) )
+         RETURN
       END IF
 *
 *     set noe = 1 if n is odd. if n is even set noe=0
