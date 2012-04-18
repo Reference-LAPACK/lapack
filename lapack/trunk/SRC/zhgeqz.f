a46051fceb45217897d73bde49a5b2f942a72e3d
julie <julie@8a072113-8704-0410-8d35-dd094bca7971>
2012-04-18 16:37:50 +0000

    Update date and revision number

--- lapack/trunk/SRC/zhgeqz.f
+++ lapack/trunk/SRC/zhgeqz.f
@@ -266,7 +266,7 @@
 *> \author Univ. of Colorado Denver 
 *> \author NAG Ltd. 
 *
-*> \date November 2011
+*> \date April 2012
 *
 *> \ingroup complex16GEcomputational
 *
@@ -284,10 +284,10 @@
      $                   ALPHA, BETA, Q, LDQ, Z, LDZ, WORK, LWORK,
      $                   RWORK, INFO )
 *
-*  -- LAPACK computational routine (version 3.4.0) --
+*  -- LAPACK computational routine (version 3.4.1) --
 *  -- LAPACK is a software package provided by Univ. of Tennessee,    --
 *  -- Univ. of California Berkeley, Univ. of Colorado Denver and NAG Ltd..--
-*     November 2011
+*     April 2012
 *
 *     .. Scalar Arguments ..
       CHARACTER          COMPQ, COMPZ, JOB
