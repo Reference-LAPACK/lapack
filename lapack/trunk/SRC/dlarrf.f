519554a0de74901fc1ca19e99fc2d13397c61e3c
james <james@8a072113-8704-0410-8d35-dd094bca7971>
2012-04-19 15:30:39 +0000

    put FOUR parameter back since it is used in this routine

--- lapack/trunk/SRC/dlarrf.f
+++ lapack/trunk/SRC/dlarrf.f
@@ -210,8 +210,8 @@
 *  =====================================================================
 *
 *     .. Parameters ..
-      DOUBLE PRECISION   MAXGROWTH1, MAXGROWTH2, ONE, QUART, TWO
-      PARAMETER          ( ONE = 1.0D0, TWO = 2.0D0,
+      DOUBLE PRECISION   FOUR, MAXGROWTH1, MAXGROWTH2, ONE, QUART, TWO
+      PARAMETER          ( ONE = 1.0D0, TWO = 2.0D0, FOUR = 4.0D0,
      $                     QUART = 0.25D0,
      $                     MAXGROWTH1 = 8.D0,
      $                     MAXGROWTH2 = 8.D0 )
