fedbb8e2a95d13dc7efd5b035cd8c746a68fa54c
julie <julie@8a072113-8704-0410-8d35-dd094bca7971>
2013-11-19 09:52:10 +0000

    Upadte version and date

--- lapack/trunk/TESTING/LIN/zerrsyx.f
+++ lapack/trunk/TESTING/LIN/zerrsyx.f
@@ -51,17 +51,17 @@
 *> \author Univ. of Colorado Denver 
 *> \author NAG Ltd. 
 *
-*> \date November 2011
+*> \date November 2013
 *
 *> \ingroup complex16_lin
 *
 *  =====================================================================
       SUBROUTINE ZERRSY( PATH, NUNIT )
 *
-*  -- LAPACK test routine (version 3.4.0) --
+*  -- LAPACK test routine (version 3.5.0) --
 *  -- LAPACK is a software package provided by Univ. of Tennessee,    --
 *  -- Univ. of California Berkeley, Univ. of Colorado Denver and NAG Ltd..--
-*     November 2011
+*     November 2013
 *
 *     .. Scalar Arguments ..
       CHARACTER*3        PATH
