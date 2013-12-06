10f3da65e078b29b72d2ebf588b1c84bc167bb63
julie <julie@8a072113-8704-0410-8d35-dd094bca7971>
2013-12-06 03:58:29 +0000

    Require CMake >= 2.8.7 so we can use CMAKE_GNUtoMS

--- lapack/trunk/lapack_build.cmake
+++ lapack/trunk/lapack_build.cmake
@@ -1,4 +1,4 @@
-cmake_minimum_required(VERSION 2.8)
+cmake_minimum_required(VERSION 2.8.7)
 ###################################################################
 # The values in this section must always be provided
 ###################################################################
