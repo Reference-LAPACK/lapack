1bbb1e4c6f52139e7402d3bf9793f249392aff2a
julie <julielangou@users.noreply.github.com>
2015-05-06 04:30:24 +0000

    Correct typo in variable name - reported by Elena (Oracle)

--- TESTING/LIN/derrvxx.f
+++ TESTING/LIN/derrvxx.f
@@ -878,13 +878,13 @@
          INFOT = 10
          CALL DSYSVXX( 'F', 'U', 2, 0, A, 2, AF, 2, IP, 'A', R, B, 2, X,
      $        2, RCOND, RPVGRW, BERR, N_ERR_BNDS, ERR_BNDS_N,
-     $        ERR_BNDS_C, NPARAMS, PARAMS, W, RW, INFO )
+     $        ERR_BNDS_C, NPARAMS, PARAMS, W, IW, INFO )
          CALL CHKXER( 'DSYSVXX', INFOT, NOUT, LERR, OK )
          INFOT = 11
          EQ='Y'
          CALL DSYSVXX( 'F', 'U', 2, 0, A, 2, AF, 2, IP, EQ, R, B, 2, X,
      $        2, RCOND, RPVGRW, BERR, N_ERR_BNDS, ERR_BNDS_N,
-     $        ERR_BNDS_C, NPARAMS, PARAMS, W, RW, INFO )
+     $        ERR_BNDS_C, NPARAMS, PARAMS, W, IW, INFO )
          CALL CHKXER( 'DSYSVXX', INFOT, NOUT, LERR, OK )
          INFOT = 13
          EQ = 'N'
