d229d60ce6978971b93f918a3429f9e91c6dc4bb
julie <julie@8a072113-8704-0410-8d35-dd094bca7971>
2015-06-25 05:09:58 +0000

    Finish removing unnecessary code - TMP = DBLE(M) - see 1547 - Thank you Elena

--- lapack/trunk/TESTING/MATGEN/dlahilb.f
+++ lapack/trunk/TESTING/MATGEN/dlahilb.f
@@ -202,7 +202,6 @@ C> \brief \b DLAHILB
 
 *     Generate matrix B as simply the first NRHS columns of M * the
 *     identity.
-      TMP = DBLE(M)
       CALL DLASET('Full', N, NRHS, 0.0D+0, DBLE(M), B, LDB)
 
 *     Generate the true solutions in X.  Because B = the first NRHS
