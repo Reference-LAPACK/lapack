7408563baae30fc71d36ade2c059395f75f5eb38
julie <julie@8a072113-8704-0410-8d35-dd094bca7971>
2016-02-23 05:36:56 +0000

    APPLYING INTEL PATCHES sent to Julie on Feb 19th 2016 by Dima from INTEL (dmitry.g.baksheev@intel.com)
    [PATCH 25/42] Fix lapacke_?gejsv - do not transpose U,V on input
    
    - U,V are [out], so no need to transpose on input

--- lapack/trunk/LAPACKE/src/lapacke_dgejsv_work.c
+++ lapack/trunk/LAPACKE/src/lapacke_dgejsv_work.c
@@ -99,14 +99,6 @@ lapack_int LAPACKE_dgejsv_work( int matrix_layout, char joba, char jobu,
         }
         /* Transpose input matrices */
         LAPACKE_dge_trans( matrix_layout, m, n, a, lda, a_t, lda_t );
-        if( LAPACKE_lsame( jobu, 'f' ) || LAPACKE_lsame( jobu, 'u' ) ||
-            LAPACKE_lsame( jobu, 'w' ) ) {
-            LAPACKE_dge_trans( matrix_layout, nu, n, u, ldu, u_t, ldu_t );
-        }
-        if( LAPACKE_lsame( jobv, 'j' ) || LAPACKE_lsame( jobv, 'v' ) ||
-            LAPACKE_lsame( jobv, 'w' ) ) {
-            LAPACKE_dge_trans( matrix_layout, nv, n, v, ldv, v_t, ldv_t );
-        }
         /* Call LAPACK function and adjust info */
         LAPACK_dgejsv( &joba, &jobu, &jobv, &jobr, &jobt, &jobp, &m, &n, a_t,
                        &lda_t, sva, u_t, &ldu_t, v_t, &ldv_t, work, &lwork,
