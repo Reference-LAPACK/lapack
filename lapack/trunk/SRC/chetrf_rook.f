04df190a9e3348cb2391d9e57d8dce485bbb3349
julie <julie@8a072113-8704-0410-8d35-dd094bca7971>
2016-02-23 05:05:07 +0000

    APPLYING INTEL PATCHES sent to Julie on Feb 19th 2016 by Dima from INTEL (dmitry.g.baksheev@intel.com)
    Subject: [PATCH 04/42] Fix ?{HE,SY}TRF_ROOK - LQUERY shall return LWORK at least 1

--- lapack/trunk/SRC/chetrf_rook.f
+++ lapack/trunk/SRC/chetrf_rook.f
@@ -265,7 +265,7 @@
 *        Determine the block size
 *
          NB = ILAENV( 1, 'CHETRF_ROOK', UPLO, N, -1, -1, -1 )
-         LWKOPT = N*NB
+         LWKOPT = MAX( 1, N*NB )
          WORK( 1 ) = LWKOPT
       END IF
 *
--- lapack/trunk/SRC/ssytrf_rook.f
+++ lapack/trunk/SRC/ssytrf_rook.f
@@ -261,7 +261,7 @@
 *        Determine the block size
 *
          NB = ILAENV( 1, 'SSYTRF_ROOK', UPLO, N, -1, -1, -1 )
-         LWKOPT = N*NB
+         LWKOPT = MAX( 1, N*NB )
          WORK( 1 ) = LWKOPT
       END IF
 *
