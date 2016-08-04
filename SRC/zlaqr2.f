3c41621216cdb2d97b6fd42ce3037eacaa5ec146
Vladimir Chalupecky <vladimir.chalupecky@gmail.com>
2016-08-04 17:52:18 +0900

    ?LAQR2: fix documentation about updated block of Z

--- SRC/zlaqr2.f
+++ SRC/zlaqr2.f
@@ -141,7 +141,7 @@
 *>          Z is COMPLEX*16 array, dimension (LDZ,N)
 *>          IF WANTZ is .TRUE., then on output, the unitary
 *>          similarity transformation mentioned above has been
-*>          accumulated into Z(ILOZ:IHIZ,ILO:IHI) from the right.
+*>          accumulated into Z(ILOZ:IHIZ,ILOZ:IHIZ) from the right.
 *>          If WANTZ is .FALSE., then Z is unreferenced.
 *> \endverbatim
 *>
