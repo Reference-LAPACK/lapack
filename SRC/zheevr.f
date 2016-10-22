773e5be4ce510da4a1400b87bfbe42dc9f502d6a
OAM <oamarques@users.noreply.github.com>
2016-10-22 12:08:44 -0700

    Update zheevr.f

--- SRC/zheevr.f
+++ SRC/zheevr.f
@@ -257,7 +257,9 @@
 *>          The support of the eigenvectors in Z, i.e., the indices
 *>          indicating the nonzero elements in Z. The i-th eigenvector
 *>          is nonzero only in elements ISUPPZ( 2*i-1 ) through
-*>          ISUPPZ( 2*i ).
+*>          ISUPPZ( 2*i ). This is an output of ZSTEMR (tridiagonal
+*>          matrix). The support of the eigenvectors of A is typically 
+*>          1:N because of the unitary transformations applied by ZUNMTR.
 *>          Implemented only for RANGE = 'A' or 'I' and IU - IL = N - 1
 *> \endverbatim
 *>
@@ -624,7 +626,7 @@
      $                   IWORK, LIWORK, INFO )
 *
 *           Apply unitary matrix used in reduction to tridiagonal
-*           form to eigenvectors returned by ZSTEIN.
+*           form to eigenvectors returned by ZSTEMR.
 *
             IF( WANTZ .AND. INFO.EQ.0 ) THEN
                INDWKN = INDWK
