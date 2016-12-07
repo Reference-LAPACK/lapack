2d36cf8eca335907855a9b203b00c8a8d5f8ec26
Renegade <Renegate@Renegates-MacBook-Pro.local>
2016-12-07 15:07:11 -0500

    removing GOTO

--- SRC/dsb2st_kernels.f
+++ SRC/dsb2st_kernels.f
@@ -1,6 +1,6 @@
 *> \brief \b DSB2ST_KERNELS
 *
-*  @generated from zhb2st_kernels.f, fortran z -> d, Sun Nov  6 19:34:06 2016
+*  @generated from zhb2st_kernels.f, fortran z -> d, Wed Dec  7 08:22:39 2016
 *      
 *  =========== DOCUMENTATION ===========
 *
@@ -128,7 +128,7 @@
 *
       IMPLICIT NONE
 *
-*  -- LAPACK computational routine (version 3.4.0) --
+*  -- LAPACK computational routine (version 3.7.0) --
 *  -- LAPACK is a software package provided by Univ. of Tennessee,    --
 *  -- Univ. of California Berkeley, Univ. of Colorado Denver and NAG Ltd..--
 *     November 2016
@@ -181,9 +181,9 @@
 
 *
 *     Upper case
-*  
+*
       IF( UPPER ) THEN
-*      
+*
           IF( WANTZ ) THEN
               VPOS   = MOD( SWEEP-1, 2 ) * N + ST
               TAUPOS = MOD( SWEEP-1, 2 ) * N + ST
@@ -191,59 +191,67 @@
               VPOS   = MOD( SWEEP-1, 2 ) * N + ST
               TAUPOS = MOD( SWEEP-1, 2 ) * N + ST
           ENDIF
-          GO TO ( 101, 102, 103 ) TTYPE
 *
-  101     CONTINUE
-          LM = ED - ST + 1
-*
-          V( VPOS ) = ONE
-          DO 10 I = 1, LM-1
-              V( VPOS+I )         = ( A( OFDPOS-I, ST+I ) )
-              A( OFDPOS-I, ST+I ) = ZERO  
-   10     CONTINUE
-          CTMP = ( A( OFDPOS, ST ) )
-          CALL DLARFG( LM, CTMP, V( VPOS+1 ), 1, 
-     $                                   TAU( TAUPOS ) )
-          A( OFDPOS, ST ) = CTMP
-* 
-  103     CONTINUE
-          LM = ED - ST + 1
-          CALL DLARFY( UPLO, LM, V( VPOS ), 1, ( TAU( TAUPOS ) ),
-     $                             A( DPOS, ST ), LDA-1, WORK)
-          GOTO 300
-*
-  102     CONTINUE
-          J1 = ED+1
-          J2 = MIN( ED+NB, N )
-          LN = ED-ST+1
-          LM = J2-J1+1
-          IF( LM.GT.0) THEN
-              CALL DLARFX( 'Left', LN, LM, V( VPOS ),
-     $                     ( TAU( TAUPOS ) ), A( DPOS-NB, J1 ),
-     $                     LDA-1, WORK)
-*
-              IF( WANTZ ) THEN
-                  VPOS   = MOD( SWEEP-1, 2 ) * N + J1
-                  TAUPOS = MOD( SWEEP-1, 2 ) * N + J1
-              ELSE
-                  VPOS   = MOD( SWEEP-1, 2 ) * N + J1
-                  TAUPOS = MOD( SWEEP-1, 2 ) * N + J1
-              ENDIF
+          IF( TTYPE.EQ.1 ) THEN
+              LM = ED - ST + 1
 *
               V( VPOS ) = ONE
-              DO 30 I = 1, LM-1
-                  V( VPOS+I )          = ( A( DPOS-NB-I, J1+I ) )
-                  A( DPOS-NB-I, J1+I ) = ZERO
-   30         CONTINUE
-              CTMP = ( A( DPOS-NB, J1 ) )
-              CALL DLARFG( LM, CTMP, V( VPOS+1 ), 1, TAU( TAUPOS ) )
-              A( DPOS-NB, J1 ) = CTMP
-*             
-              CALL DLARFX( 'Right', LN-1, LM, V( VPOS ), 
-     $                     TAU( TAUPOS ),
-     $                     A( DPOS-NB+1, J1 ), LDA-1, WORK)
+              DO 10 I = 1, LM-1
+                  V( VPOS+I )         = ( A( OFDPOS-I, ST+I ) )
+                  A( OFDPOS-I, ST+I ) = ZERO  
+   10         CONTINUE
+              CTMP = ( A( OFDPOS, ST ) )
+              CALL DLARFG( LM, CTMP, V( VPOS+1 ), 1, 
+     $                                       TAU( TAUPOS ) )
+              A( OFDPOS, ST ) = CTMP
+*
+              LM = ED - ST + 1
+              CALL DLARFY( UPLO, LM, V( VPOS ), 1,
+     $                     ( TAU( TAUPOS ) ),
+     $                     A( DPOS, ST ), LDA-1, WORK)
           ENDIF
-          GOTO 300
+*
+          IF( TTYPE.EQ.3 ) THEN
+*
+              LM = ED - ST + 1
+              CALL DLARFY( UPLO, LM, V( VPOS ), 1,
+     $                     ( TAU( TAUPOS ) ),
+     $                     A( DPOS, ST ), LDA-1, WORK)
+          ENDIF
+*
+          IF( TTYPE.EQ.2 ) THEN
+              J1 = ED+1
+              J2 = MIN( ED+NB, N )
+              LN = ED-ST+1
+              LM = J2-J1+1
+              IF( LM.GT.0) THEN
+                  CALL DLARFX( 'Left', LN, LM, V( VPOS ),
+     $                         ( TAU( TAUPOS ) ),
+     $                         A( DPOS-NB, J1 ), LDA-1, WORK)
+*
+                  IF( WANTZ ) THEN
+                      VPOS   = MOD( SWEEP-1, 2 ) * N + J1
+                      TAUPOS = MOD( SWEEP-1, 2 ) * N + J1
+                  ELSE
+                      VPOS   = MOD( SWEEP-1, 2 ) * N + J1
+                      TAUPOS = MOD( SWEEP-1, 2 ) * N + J1
+                  ENDIF
+*
+                  V( VPOS ) = ONE
+                  DO 30 I = 1, LM-1
+                      V( VPOS+I )          = 
+     $                                    ( A( DPOS-NB-I, J1+I ) )
+                      A( DPOS-NB-I, J1+I ) = ZERO
+   30             CONTINUE
+                  CTMP = ( A( DPOS-NB, J1 ) )
+                  CALL DLARFG( LM, CTMP, V( VPOS+1 ), 1, TAU( TAUPOS ) )
+                  A( DPOS-NB, J1 ) = CTMP
+*                 
+                  CALL DLARFX( 'Right', LN-1, LM, V( VPOS ),
+     $                         TAU( TAUPOS ),
+     $                         A( DPOS-NB+1, J1 ), LDA-1, WORK)
+              ENDIF
+          ENDIF
 *
 *     Lower case
 *  
@@ -256,63 +264,70 @@
               VPOS   = MOD( SWEEP-1, 2 ) * N + ST
               TAUPOS = MOD( SWEEP-1, 2 ) * N + ST
           ENDIF
-          GO TO ( 201, 202, 203 ) TTYPE
-*  
-  201     CONTINUE
-          LM = ED - ST + 1
 *
-          V( VPOS ) = ONE
-          DO 20 I = 1, LM-1
-              V( VPOS+I )         = A( OFDPOS+I, ST-1 )
-              A( OFDPOS+I, ST-1 ) = ZERO  
-   20     CONTINUE
-          CALL DLARFG( LM, A( OFDPOS, ST-1 ), V( VPOS+1 ), 1, 
-     $                                   TAU( TAUPOS ) )
+          IF( TTYPE.EQ.1 ) THEN
+              LM = ED - ST + 1
 *
-  203     CONTINUE
-          LM = ED - ST + 1
-*
-          CALL DLARFY( UPLO, LM, V( VPOS ), 1, ( TAU( TAUPOS ) ),
-     $                                      A( DPOS, ST ), LDA-1, WORK)
-
-          GOTO 300
-*
-  202     CONTINUE
-          J1 = ED+1
-          J2 = MIN( ED+NB, N )
-          LN = ED-ST+1
-          LM = J2-J1+1
-*
-          IF( LM.GT.0) THEN
-              CALL DLARFX( 'Right', LM, LN, V( VPOS ), 
-     $                     TAU( TAUPOS ), A( DPOS+NB, ST ),
-     $                     LDA-1, WORK)
-*
-              IF( WANTZ ) THEN
-                  VPOS   = MOD( SWEEP-1, 2 ) * N + J1
-                  TAUPOS = MOD( SWEEP-1, 2 ) * N + J1
-              ELSE
-                  VPOS   = MOD( SWEEP-1, 2 ) * N + J1
-                  TAUPOS = MOD( SWEEP-1, 2 ) * N + J1
-              ENDIF
-*              
               V( VPOS ) = ONE
-              DO 40 I = 1, LM-1
-                  V( VPOS+I )        = A( DPOS+NB+I, ST )
-                  A( DPOS+NB+I, ST ) = ZERO
-   40         CONTINUE
-              CALL DLARFG( LM, A( DPOS+NB, ST ), V( VPOS+1 ), 1, 
-     $                                    TAU( TAUPOS ) )
-*                  
-              CALL DLARFX( 'Left', LM, LN-1, V( VPOS ), 
+              DO 20 I = 1, LM-1
+                  V( VPOS+I )         = A( OFDPOS+I, ST-1 )
+                  A( OFDPOS+I, ST-1 ) = ZERO  
+   20         CONTINUE
+              CALL DLARFG( LM, A( OFDPOS, ST-1 ), V( VPOS+1 ), 1, 
+     $                                       TAU( TAUPOS ) )
+*
+              LM = ED - ST + 1
+*
+              CALL DLARFY( UPLO, LM, V( VPOS ), 1,
      $                     ( TAU( TAUPOS ) ),
-     $                     A( DPOS+NB-1, ST+1 ), LDA-1, WORK)
+     $                     A( DPOS, ST ), LDA-1, WORK)
 
           ENDIF
-          GOTO 300
-      ENDIF
+*
+          IF( TTYPE.EQ.3 ) THEN
+              LM = ED - ST + 1
+*
+              CALL DLARFY( UPLO, LM, V( VPOS ), 1,
+     $                     ( TAU( TAUPOS ) ),
+     $                     A( DPOS, ST ), LDA-1, WORK)
 
-  300 CONTINUE    
+          ENDIF
+*
+          IF( TTYPE.EQ.2 ) THEN
+              J1 = ED+1
+              J2 = MIN( ED+NB, N )
+              LN = ED-ST+1
+              LM = J2-J1+1
+*
+              IF( LM.GT.0) THEN
+                  CALL DLARFX( 'Right', LM, LN, V( VPOS ), 
+     $                         TAU( TAUPOS ), A( DPOS+NB, ST ),
+     $                         LDA-1, WORK)
+*
+                  IF( WANTZ ) THEN
+                      VPOS   = MOD( SWEEP-1, 2 ) * N + J1
+                      TAUPOS = MOD( SWEEP-1, 2 ) * N + J1
+                  ELSE
+                      VPOS   = MOD( SWEEP-1, 2 ) * N + J1
+                      TAUPOS = MOD( SWEEP-1, 2 ) * N + J1
+                  ENDIF
+*
+                  V( VPOS ) = ONE
+                  DO 40 I = 1, LM-1
+                      V( VPOS+I )        = A( DPOS+NB+I, ST )
+                      A( DPOS+NB+I, ST ) = ZERO
+   40             CONTINUE
+                  CALL DLARFG( LM, A( DPOS+NB, ST ), V( VPOS+1 ), 1, 
+     $                                        TAU( TAUPOS ) )
+*
+                  CALL DLARFX( 'Left', LM, LN-1, V( VPOS ), 
+     $                         ( TAU( TAUPOS ) ),
+     $                         A( DPOS+NB-1, ST+1 ), LDA-1, WORK)
+             
+              ENDIF
+          ENDIF
+      ENDIF
+*
       RETURN
 *
 *     END OF DSB2ST_KERNELS
