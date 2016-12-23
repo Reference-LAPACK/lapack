96ea435363f33f406a8b3db5dd6d700890186f66
Julie <julie@cs.utk.edu>
2016-12-23 13:33:38 -0800

    Updating version on Version files

--- SRC/ilaver.f
+++ SRC/ilaver.f
@@ -47,14 +47,14 @@
 *> \author Univ. of Colorado Denver
 *> \author NAG Ltd.
 *
-*> \date June 2016
+*> \date December 2016
 *
 *> \ingroup OTHERauxiliary
 *
 *  =====================================================================
       SUBROUTINE ILAVER( VERS_MAJOR, VERS_MINOR, VERS_PATCH )
 *
-*  -- LAPACK computational routine (version 3.6.1) --
+*  -- LAPACK computational routine (version 3.7.0) --
 *  -- LAPACK is a software package provided by Univ. of Tennessee,    --
 *  -- Univ. of California Berkeley, Univ. of Colorado Denver and NAG Ltd..--
 *     June 2016
@@ -64,8 +64,8 @@
       INTEGER VERS_MAJOR, VERS_MINOR, VERS_PATCH
 *  =====================================================================
       VERS_MAJOR = 3
-      VERS_MINOR = 6
-      VERS_PATCH = 1
+      VERS_MINOR = 7
+      VERS_PATCH = 0
 *  =====================================================================
 *
       RETURN
