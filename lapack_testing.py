Reference-LAPACK/lapack

commit ed4e95e4d25f5f995db75445cbc2ba1db5736d23

First minor change to 2 stage contribution
Update Makefile to handle .F

Update Cmake build
Note: We have duplicate loop numbers due to Define statement that stops
the compilation with Cmake default option.
Fix coming very shortly

lapack_testing.py
@@ -226,19 +226,19 @@ for dtype in range_prec:
         sys.stdout.flush()
 
     dtests = (
-    ("nep", "sep", "svd",
+    ("nep", "sep", "se2", "svd",
     letter+"ec",letter+"ed",letter+"gg",
     letter+"gd",letter+"sb",letter+"sg",
     letter+"bb","glm","gqr",
     "gsv","csd","lse",
     letter+"test", letter+dtypes[0][dtype-1]+"test",letter+"test_rfp"),
-    ("Nonsymmetric Eigenvalue Problem", "Symmetric Eigenvalue Problem", "Singular Value Decomposition",
+    ("Nonsymmetric Eigenvalue Problem", "Symmetric Eigenvalue Problem", "Symmetric Eigenvalue Problem 2 stage", "Singular Value Decomposition",
     "Eigen Condition","Nonsymmetric Eigenvalue","Nonsymmetric Generalized Eigenvalue Problem",
     "Nonsymmetric Generalized Eigenvalue Problem driver", "Symmetric Eigenvalue Problem", "Symmetric Eigenvalue Generalized Problem",
     "Banded Singular Value Decomposition routines", "Generalized Linear Regression Model routines", "Generalized QR and RQ factorization routines",
     "Generalized Singular Value Decomposition routines", "CS Decomposition routines", "Constrained Linear Least Squares routines",
     "Linear Equation routines", "Mixed Precision linear equation routines","RFP linear equation routines"),
-    (letter+"nep", letter+"sep", letter+"svd",
+    (letter+"nep", letter+"sep", letter+"se2", letter+"svd",
     letter+"ec",letter+"ed",letter+"gg",
     letter+"gd",letter+"sb",letter+"sg",
     letter+"bb",letter+"glm",letter+"gqr",

SRC/CMakeLists.txt
@@ -46,7 +46,7 @@
 #
 #######################################################################
 
-set(ALLAUX  ilaenv.f ieeeck.f lsamen.f  iparmq.f
+set(ALLAUX  ilaenv.f ieeeck.f lsamen.f  iparmq.f iparam2stage.f
     ilaprec.f ilatrans.f ilauplo.f iladiag.f chla_transtype.f
     ../INSTALL/ilaver.f ../INSTALL/lsame.f xerbla.f xerbla_array.f
     ../INSTALL/slamch.f)
@@ -112,7 +112,7 @@ set(SLASRC
    slaqgb.f slaqge.f slaqp2.f slaqps.f slaqsb.f slaqsp.f slaqsy.f
    slaqr0.f slaqr1.f slaqr2.f slaqr3.f slaqr4.f slaqr5.f
    slaqtr.f slar1v.f slar2v.f ilaslr.f ilaslc.f
-   slarf.f  slarfb.f slarfg.f slarfgp.f slarft.f slarfx.f slargv.f
+   slarf.f  slarfb.f slarfg.f slarfgp.f slarft.f slarfx.f slarfy.f slargv.f
    slarrv.f slartv.f
    slarz.f  slarzb.f slarzt.f slaswp.f slasy2.f 
    slasyf.f slasyf_rook.f slasyf_rk.f slasyf_aa.f
@@ -160,7 +160,10 @@ set(SLASRC
    sgelqt.f sgelqt3.f sgemlqt.f
    sgetsls.f sgeqr.f slatsqr.f slamtsqr.f sgemqr.f
    sgelq.f slaswlq.f slamswlq.f sgemlq.f
-   stplqt.f stplqt2.f stpmlqt.f)
+   stplqt.f stplqt2.f stpmlqt.f
+   ssytrd_2stage.f ssytrd_sy2sb.f ssytrd_sb2st.f ssb2st_kernels.f
+   ssyevd_2stage.f ssyev_2stage.f ssyevx_2stage.f ssyevr_2stage.f
+   ssbev_2stage.f ssbevx_2stage.f ssbevd_2stage.f ssygv_2stage.f)
 
 set(DSLASRC spotrs.f sgetrs.f spotrf.f sgetrf.f)
 
@@ -215,7 +218,7 @@ set(CLASRC
    claqr0.f claqr1.f claqr2.f claqr3.f claqr4.f claqr5.f
    claqsp.f claqsy.f clar1v.f clar2v.f ilaclr.f ilaclc.f
    clarf.f  clarfb.f clarfg.f clarfgp.f clarft.f
-   clarfx.f clargv.f clarnv.f clarrv.f clartg.f clartv.f
+   clarfx.f clarfy.f clargv.f clarnv.f clarrv.f clartg.f clartv.f
    clarz.f  clarzb.f clarzt.f clascl.f claset.f clasr.f  classq.f
    claswp.f clasyf.f clasyf_rook.f clasyf_rk.f clatbs.f clatdf.f clatps.f clatrd.f clatrs.f clatrz.f
    clauu2.f clauum.f cpbcon.f cpbequ.f cpbrfs.f cpbstf.f cpbsv.f
@@ -253,7 +256,10 @@ set(CLASRC
    cgelqt.f cgelqt3.f cgemlqt.f
    cgetsls.f cgeqr.f clatsqr.f clamtsqr.f cgemqr.f
    cgelq.f claswlq.f clamswlq.f cgemlq.f
-   ctplqt.f ctplqt2.f ctpmlqt.f)
+   ctplqt.f ctplqt2.f ctpmlqt.f
+   chetrd_2stage.f chetrd_he2hb.f chetrd_hb2st.f chb2st_kernels.f
+   cheevd_2stage.f cheev_2stage.f cheevx_2stage.f cheevr_2stage.f
+   chbev_2stage.f chbevx_2stage.f chbevd_2stage.f chegv_2stage.f)
 
 set(CXLASRC     cgesvxx.f cgerfsx.f cla_gerfsx_extended.f cla_geamv.f
    cla_gercond_c.f cla_gercond_x.f cla_gerpvgrw.f
@@ -292,8 +298,8 @@ set(DLASRC
    dlaqgb.f dlaqge.f dlaqp2.f dlaqps.f dlaqsb.f dlaqsp.f dlaqsy.f
    dlaqr0.f dlaqr1.f dlaqr2.f dlaqr3.f dlaqr4.f dlaqr5.f
    dlaqtr.f dlar1v.f dlar2v.f iladlr.f iladlc.f
-   dlarf.f  dlarfb.f dlarfg.f dlarfgp.f dlarft.f dlarfx.f dlargv.f
-   dlarrv.f dlartv.f
+   dlarf.f  dlarfb.f dlarfg.f dlarfgp.f dlarft.f dlarfx.f dlarfy.f
+   dlargv.f dlarrv.f dlartv.f
    dlarz.f  dlarzb.f dlarzt.f dlaswp.f dlasy2.f 
    dlasyf.f dlasyf_rook.f dlasyf_rk.f dlasyf_aa.f
    dlatbs.f dlatdf.f dlatps.f dlatrd.f dlatrs.f dlatrz.f dlauu2.f
@@ -341,7 +347,10 @@ set(DLASRC
    dgelqt.f dgelqt3.f dgemlqt.f
    dgetsls.f dgeqr.f dlatsqr.f dlamtsqr.f dgemqr.f
    dgelq.f dlaswlq.f dlamswlq.f dgemlq.f
-   dtplqt.f dtplqt2.f dtpmlqt.f )
+   dtplqt.f dtplqt2.f dtpmlqt.f
+   dsytrd_2stage.f dsytrd_sy2sb.f dsytrd_sb2st.f dsb2st_kernels.f
+   dsyevd_2stage.f dsyev_2stage.f dsyevx_2stage.f dsyevr_2stage.f
+   dsbev_2stage.f dsbevx_2stage.f dsbevd_2stage.f dsygv_2stage.f)
 
 set(DXLASRC dgesvxx.f dgerfsx.f dla_gerfsx_extended.f dla_geamv.f
    dla_gercond.f dla_gerpvgrw.f dsysvxx.f dsyrfsx.f
@@ -396,7 +405,7 @@ set(ZLASRC
    zlaqsp.f zlaqsy.f zlar1v.f zlar2v.f ilazlr.f ilazlc.f
    zlarcm.f zlarf.f  zlarfb.f
    zlarfg.f zlarfgp.f zlarft.f
-   zlarfx.f zlargv.f zlarnv.f zlarrv.f zlartg.f zlartv.f
+   zlarfx.f zlarfy.f zlargv.f zlarnv.f zlarrv.f zlartg.f zlartv.f
    zlarz.f  zlarzb.f zlarzt.f zlascl.f zlaset.f zlasr.f
    zlassq.f zlaswp.f zlasyf.f zlasyf_rook.f zlasyf_rk.f
    zlatbs.f zlatdf.f zlatps.f zlatrd.f zlatrs.f zlatrz.f zlauu2.f
@@ -438,7 +447,10 @@ set(ZLASRC
    zgelqt.f zgelqt3.f zgemlqt.f
    zgetsls.f zgeqr.f zlatsqr.f zlamtsqr.f zgemqr.f
    zgelq.f zlaswlq.f zlamswlq.f zgemlq.f
-   ztplqt.f ztplqt2.f ztpmlqt.f)
+   ztplqt.f ztplqt2.f ztpmlqt.f
+   zhetrd_2stage.f zhetrd_he2hb.f zhetrd_hb2st.f zhb2st_kernels.f
+   zheevd_2stage.f zheev_2stage.f zheevx_2stage.f zheevr_2stage.f
+   zhbev_2stage.f zhbevx_2stage.f zhbevd_2stage.f zhegv_2stage.f)
 
 set(ZXLASRC  zgesvxx.f zgerfsx.f zla_gerfsx_extended.f zla_geamv.f
    zla_gercond_c.f zla_gercond_x.f zla_gerpvgrw.f zsysvxx.f zsyrfsx.f

SRC/Makefile
@@ -556,6 +556,9 @@ clean:
 .f.o:
 	$(FORTRAN) $(OPTS) -c $< -o $@
 
+.F.o:
+	$(FORTRAN) $(OPTS) -c $< -o $@
+	
 slaruv.o: slaruv.f ; $(FORTRAN) $(NOOPT) -c $< -o $@
 dlaruv.o: dlaruv.f ; $(FORTRAN) $(NOOPT) -c $< -o $@
 sla_wwaddw.o: sla_wwaddw.f ; $(FORTRAN) $(NOOPT) -c $< -o $@

SRC/ilaenv.f
@@ -132,7 +132,7 @@
 *> \author Univ. of Colorado Denver
 *> \author NAG Ltd.
 *
-*> \date June 2016
+*> \date November 2016
 *
 *> \ingroup OTHERauxiliary
 *
@@ -162,10 +162,10 @@
 *  =====================================================================
       INTEGER FUNCTION ILAENV( ISPEC, NAME, OPTS, N1, N2, N3, N4 )
 *
-*  -- LAPACK auxiliary routine (version 3.6.1) --
+*  -- LAPACK auxiliary routine (version 3.7.0) --
 *  -- LAPACK is a software package provided by Univ. of Tennessee,    --
 *  -- Univ. of California Berkeley, Univ. of Colorado Denver and NAG Ltd..--
-*     June 2016
+*     November 2016
 *
 *     .. Scalar Arguments ..
       CHARACTER*( * )    NAME, OPTS
@@ -183,8 +183,8 @@
       INTRINSIC          CHAR, ICHAR, INT, MIN, REAL
 *     ..
 *     .. External Functions ..
-      INTEGER            IEEECK, IPARMQ
-      EXTERNAL           IEEECK, IPARMQ
+      INTEGER            IEEECK, IPARMQ, IPARAM2STAGE
+      EXTERNAL           IEEECK, IPARMQ, IPARAM2STAGE
 *     ..
 *     .. Executable Statements ..
 *

TESTING/CMakeLists.txt
@@ -37,6 +37,9 @@ add_lapack_test(snep.out nep.in xeigtsts)
 add_lapack_test(ssep.out sep.in xeigtsts)
 
 
+add_lapack_test(sse2.out se2.in xeigtsts)
+
+
 add_lapack_test(ssvd.out svd.in xeigtsts)
 
 
@@ -104,6 +107,9 @@ add_lapack_test(dnep.out nep.in xeigtstd)
 add_lapack_test(dsep.out sep.in xeigtstd)
 
 
+add_lapack_test(dse2.out se2.in xeigtstd)
+
+
 add_lapack_test(dsvd.out svd.in xeigtstd)
 
 
@@ -167,6 +173,9 @@ add_lapack_test(cnep.out nep.in xeigtstc)
 
 
 add_lapack_test(csep.out sep.in xeigtstc)
+
+
+add_lapack_test(cse2.out se2.in xeigtstc)
 
 
 add_lapack_test(csvd.out svd.in xeigtstc)
@@ -234,6 +243,9 @@ add_lapack_test(znep.out nep.in xeigtstz)
 
 
 add_lapack_test(zsep.out sep.in xeigtstz)
+
+
+add_lapack_test(zse2.out se2.in xeigtstz)
 
 
 add_lapack_test(zsvd.out svd.in xeigtstz)

TESTING/EIG/CMakeLists.txt
@@ -49,11 +49,11 @@ set(SCIGTST  slafts.f slahd2.f slasum.f slatb9.f sstech.f sstect.f
 set(SEIGTST  schkee.f
    sbdt01.f sbdt02.f sbdt03.f sbdt04.f sbdt05.f
    schkbb.f schkbd.f schkbk.f schkbl.f schkec.f
-   schkgg.f schkgk.f schkgl.f schkhs.f schksb.f schkst.f
+   schkgg.f schkgk.f schkgl.f schkhs.f schksb.f schkst.f schkst2stg.f schksb2stg.f
    sckcsd.f sckglm.f sckgqr.f sckgsv.f scklse.f scsdts.f
    sdrges.f sdrgev.f sdrges3.f sdrgev3.f sdrgsx.f sdrgvx.f
-   sdrvbd.f sdrves.f sdrvev.f sdrvsg.f
-   sdrvst.f sdrvsx.f sdrvvx.f
+   sdrvbd.f sdrves.f sdrvev.f sdrvsg.f sdrvsg2stg.f
+   sdrvst.f sdrvst2stg.f sdrvsx.f sdrvvx.f
    serrbd.f serrec.f serred.f serrgg.f serrhs.f serrst.f
    sget02.f sget10.f sget22.f sget23.f sget24.f sget31.f
    sget32.f sget33.f sget34.f sget35.f sget36.f
@@ -66,11 +66,11 @@ set(SEIGTST  schkee.f
 set(CEIGTST  cchkee.f
    cbdt01.f cbdt02.f cbdt03.f cbdt05.f
    cchkbb.f cchkbd.f cchkbk.f cchkbl.f cchkec.f
-   cchkgg.f cchkgk.f cchkgl.f cchkhb.f cchkhs.f cchkst.f
+   cchkgg.f cchkgk.f cchkgl.f cchkhb.f cchkhs.f cchkst.f cchkst2stg.f cchkhb2stg.f
    cckcsd.f cckglm.f cckgqr.f cckgsv.f ccklse.f ccsdts.f
    cdrges.f cdrgev.f cdrges3.f cdrgev3.f cdrgsx.f cdrgvx.f
-   cdrvbd.f cdrves.f cdrvev.f cdrvsg.f
-   cdrvst.f cdrvsx.f cdrvvx.f
+   cdrvbd.f cdrves.f cdrvev.f cdrvsg.f cdrvsg2stg.f
+   cdrvst.f cdrvst2stg.f cdrvsx.f cdrvvx.f
    cerrbd.f cerrec.f cerred.f cerrgg.f cerrhs.f cerrst.f
    cget02.f cget10.f cget22.f cget23.f cget24.f
    cget35.f cget36.f cget37.f cget38.f cget51.f cget52.f
@@ -86,11 +86,11 @@ set(DZIGTST  dlafts.f dlahd2.f dlasum.f dlatb9.f dstech.f dstect.f
 set(DEIGTST  dchkee.f
    dbdt01.f dbdt02.f dbdt03.f dbdt04.f dbdt05.f
    dchkbb.f dchkbd.f dchkbk.f dchkbl.f dchkec.f
-   dchkgg.f dchkgk.f dchkgl.f dchkhs.f dchksb.f dchkst.f
+   dchkgg.f dchkgk.f dchkgl.f dchkhs.f dchksb.f dchkst.f dchkst2stg.f dchksb2stg.f
    dckcsd.f dckglm.f dckgqr.f dckgsv.f dcklse.f dcsdts.f
    ddrges.f ddrgev.f ddrges3.f ddrgev3.f ddrgsx.f ddrgvx.f
-   ddrvbd.f ddrves.f ddrvev.f ddrvsg.f
-   ddrvst.f ddrvsx.f ddrvvx.f
+   ddrvbd.f ddrves.f ddrvev.f ddrvsg.f ddrvsg2stg.f
+   ddrvst.f ddrvst2stg.f ddrvsx.f ddrvvx.f
    derrbd.f derrec.f derred.f derrgg.f derrhs.f derrst.f
    dget02.f dget10.f dget22.f dget23.f dget24.f dget31.f
    dget32.f dget33.f dget34.f dget35.f dget36.f
@@ -103,11 +103,11 @@ set(DEIGTST  dchkee.f
 set(ZEIGTST  zchkee.f
    zbdt01.f zbdt02.f zbdt03.f zbdt05.f
    zchkbb.f zchkbd.f zchkbk.f zchkbl.f zchkec.f
-   zchkgg.f zchkgk.f zchkgl.f zchkhb.f zchkhs.f zchkst.f
+   zchkgg.f zchkgk.f zchkgl.f zchkhb.f zchkhs.f zchkst.f zchkst2stg.f zchkhb2stg.f
    zckcsd.f zckglm.f zckgqr.f zckgsv.f zcklse.f zcsdts.f
    zdrges.f zdrgev.f zdrges3.f zdrgev3.f zdrgsx.f zdrgvx.f
-   zdrvbd.f zdrves.f zdrvev.f zdrvsg.f
-   zdrvst.f zdrvsx.f zdrvvx.f
+   zdrvbd.f zdrves.f zdrvev.f zdrvsg.f ddrvsg2stg.f
+   zdrvst.f ddrvst2stg.f zdrvsx.f zdrvvx.f
    zerrbd.f zerrec.f zerred.f zerrgg.f zerrhs.f zerrst.f
    zget02.f zget10.f zget22.f zget23.f zget24.f
    zget35.f zget36.f zget37.f zget38.f zget51.f zget52.f

TESTING/EIG/cchkhb2stg.f
@@ -1,7 +1,5 @@
 *> \brief \b CCHKHBSTG
 *
-*  @generated from zchkhb2stg.f, fortran z -> c, Sun Nov  6 00:22:35 2016
-*
 *  =========== DOCUMENTATION ===========
 *
 * Online html documentation available at
@@ -315,7 +313,7 @@
 *> \author Univ. of Colorado Denver
 *> \author NAG Ltd.
 *
-*> \date November 2011
+*> \date November 2016
 *
 *> \ingroup complex_eig
 *
@@ -325,10 +323,10 @@
      $                   D2, D3, U, LDU, WORK, LWORK, RWORK, RESULT, 
      $                   INFO )
 *
-*  -- LAPACK test routine (version 3.4.0) --
+*  -- LAPACK test routine (version 3.7.0) --
 *  -- LAPACK is a software package provided by Univ. of Tennessee,    --
 *  -- Univ. of California Berkeley, Univ. of Colorado Denver and NAG Ltd..--
-*     November 2011
+*     November 2016
 *
 *     .. Scalar Arguments ..
       INTEGER            INFO, LDA, LDU, LWORK, NOUNIT, NSIZES, NTYPES,
@@ -360,8 +358,8 @@
 *     .. Local Scalars ..
       LOGICAL            BADNN, BADNNB
       INTEGER            I, IINFO, IMODE, ITYPE, J, JC, JCOL, JR, JSIZE,
-     $                   JTYPE, JWIDTH, K, KMAX, MTYPES, N, NERRS,
-     $                   NMATS, NMAX, NTEST, NTESTT
+     $                   JTYPE, JWIDTH, K, KMAX, LH, LW, MTYPES, N,
+     $                   NERRS, NMATS, NMAX, NTEST, NTESTT
       REAL               ANINV, ANORM, COND, OVFL, RTOVFL, RTUNFL,
      $                   TEMP1, TEMP2, TEMP3, TEMP4, ULP, ULPINV, UNFL
 *     ..

TESTING/EIG/cchkst2stg.f
@@ -1,7 +1,5 @@
 *> \brief \b CCHKST2STG
 *
-*  @generated from zchkst2stg.f, fortran z -> c, Fri Nov  4 15:45:07 2016
-*
 *  =========== DOCUMENTATION ===========
 *
 * Online html documentation available at
@@ -616,7 +614,7 @@
 *> \author Univ. of Colorado Denver
 *> \author NAG Ltd.
 *
-*> \date November 2011
+*> \date November 2016
 *
 *> \ingroup complex_eig
 *
@@ -627,10 +625,10 @@
      $                   LWORK, RWORK, LRWORK, IWORK, LIWORK, RESULT,
      $                   INFO )
 *
-*  -- LAPACK test routine (version 3.4.0) --
+*  -- LAPACK test routine (version 3.7.0) --
 *  -- LAPACK is a software package provided by Univ. of Tennessee,    --
 *  -- Univ. of California Berkeley, Univ. of Colorado Denver and NAG Ltd..--
-*     November 2011
+*     November 2016
 *
 *     .. Scalar Arguments ..
       INTEGER            INFO, LDA, LDU, LIWORK, LRWORK, LWORK, NOUNIT,
@@ -1096,56 +1094,6 @@
 *
             RESULT( 3 ) = TEMP2 / MAX( UNFL, ULP*MAX( TEMP1, TEMP2 ) )
             RESULT( 4 ) = TEMP4 / MAX( UNFL, ULP*MAX( TEMP3, TEMP4 ) )
-*
-*           Skip the DSYTRD for lower that since we replaced its testing
-*           3 and 4 by the 2-stage one.
-            GOTO 101  
-*
-*           Call CHETRD and CUNGTR to compute S and U from
-*           lower triangle, do tests.
-*
-            CALL CLACPY( 'L', N, N, A, LDA, V, LDU )
-*
-            NTEST = 3
-            CALL CHETRD( 'L', N, V, LDU, SD, SE, TAU, WORK, LWORK,
-     $                   IINFO )
-*
-            IF( IINFO.NE.0 ) THEN
-               WRITE( NOUNIT, FMT = 9999 )'CHETRD(L)', IINFO, N, JTYPE,
-     $            IOLDSD
-               INFO = ABS( IINFO )
-               IF( IINFO.LT.0 ) THEN
-                  RETURN
-               ELSE
-                  RESULT( 3 ) = ULPINV
-                  GO TO 280
-               END IF
-            END IF
-*
-            CALL CLACPY( 'L', N, N, V, LDU, U, LDU )
-*
-            NTEST = 4
-            CALL CUNGTR( 'L', N, U, LDU, TAU, WORK, LWORK, IINFO )
-            IF( IINFO.NE.0 ) THEN
-               WRITE( NOUNIT, FMT = 9999 )'CUNGTR(L)', IINFO, N, JTYPE,
-     $            IOLDSD
-               INFO = ABS( IINFO )
-               IF( IINFO.LT.0 ) THEN
-                  RETURN
-               ELSE
-                  RESULT( 4 ) = ULPINV
-                  GO TO 280
-               END IF
-            END IF
-*
-            CALL CHET21( 2, 'Lower', N, 1, A, LDA, SD, SE, U, LDU, V,
-     $                   LDU, TAU, WORK, RWORK, RESULT( 3 ) )
-            CALL CHET21( 3, 'Lower', N, 1, A, LDA, SD, SE, U, LDU, V,
-     $                   LDU, TAU, WORK, RWORK, RESULT( 4 ) )
-*
-*after skipping old tests 3 4 back to the normal 
-*
-  101       CONTINUE
 *
 *           Store the upper triangle of A in AP
 *

commit 6c3b79b35e83df36e30bd2ceadc9b927a3287740

Fix -r option of lapack_testing.py.
The numbering of tests had been broken in commit ed4e95e4.

lapack_testing.py
@@ -197,16 +197,16 @@ else:
     range_prec=list(range(4))
 
 if test=='lin':
-    range_test=[15]
-elif test=='mixed':
     range_test=[16]
+elif test=='mixed':
+    range_test=[17]
     range_prec=[1,3]
 elif test=='rfp':
-    range_test=[17]
+    range_test=[18]
 elif test=='eig':
-    range_test=list(range(15))
+    range_test=list(range(16))
 else:
-    range_test=list(range(18))
+    range_test=list(range(19))
 
 list_results = [
 [0, 0, 0, 0, 0],
@@ -250,18 +250,18 @@ for dtype in range_prec:
     for dtest in range_test:
         nb_of_test=0
         # NEED TO SKIP SOME PRECISION (namely s and c) FOR PROTO MIXED PRECISION TESTING
-        if dtest==16 and (letter=="s" or letter=="c"):
+        if dtest==17 and (letter=="s" or letter=="c"):
             continue
         if (with_file==1):
             cmdbase=dtests[2][dtest]+".out"
         else:
-            if dtest==15:
+            if dtest==16:
                 # LIN TESTS
                 cmdbase="xlintst"+letter+" < "+dtests[0][dtest]+".in > "+dtests[2][dtest]+".out"
-            elif dtest==16:
+            elif dtest==17:
                 # PROTO LIN TESTS
                 cmdbase="xlintst"+letter+dtypes[0][dtype-1]+" < "+dtests[0][dtest]+".in > "+dtests[2][dtest]+".out"
-            elif dtest==17:
+            elif dtest==18:
                 # PROTO LIN TESTS
                 cmdbase="xlintstrf"+letter+" < "+dtests[0][dtest]+".in > "+dtests[2][dtest]+".out"
             else: