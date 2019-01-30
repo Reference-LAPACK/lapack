86f70bdfe4341002b81b8fbb5a63413df48f39db
sergey.kuznetsov <sergey.kuznetsov@intel.com>
2019-01-30 15:10:08 -0800

    For symmetric eigensolvers  only upper or lower triangle of a matrix should be set and  remaining  part of matrix can contain any values including NaNs but  these LAPACKE symmetric eigensolver drivers checked the whole matrix for NaNs and they used transposition  of the whole matrix. The new versions check and transpose upper or lower triangle of matrix.

--- LAPACKE/src/lapacke_cheevd_2stage.c
+++ LAPACKE/src/lapacke_cheevd_2stage.c
@@ -53,7 +53,7 @@ lapack_int LAPACKE_cheevd_2stage( int matrix_layout, char jobz, char uplo, lapac
 #ifndef LAPACK_DISABLE_NAN_CHECK
     if( LAPACKE_get_nancheck() ) {
         /* Optionally check input matrices for NaNs */
-        if( LAPACKE_cge_nancheck( matrix_layout, n, n, a, lda ) ) {
+        if( LAPACKE_che_nancheck( matrix_layout, uplo, n, a, lda ) ) {
             return -5;
         }
     }
