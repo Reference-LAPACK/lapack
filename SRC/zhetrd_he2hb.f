7939cb1b4d977da37a90c3b515f4ce4c3fe82435
Christian Friedl <friedc@users.noreply.github.com>
2022-05-29 22:15:29 +0200

    Replace empty strings with a single blank. This fixes errors when compiling with ibm xlf.

--- SRC/zhetrd_he2hb.f
+++ SRC/zhetrd_he2hb.f
@@ -293,7 +293,7 @@
       INFO   = 0
       UPPER  = LSAME( UPLO, 'U' )
       LQUERY = ( LWORK.EQ.-1 )
-      LWMIN  = ILAENV2STAGE( 4, 'ZHETRD_HE2HB', '', N, KD, -1, -1 )
+      LWMIN  = ILAENV2STAGE( 4, 'ZHETRD_HE2HB', ' ', N, KD, -1, -1 )
       
       IF( .NOT.UPPER .AND. .NOT.LSAME( UPLO, 'L' ) ) THEN
          INFO = -1
