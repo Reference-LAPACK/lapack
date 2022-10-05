6c6c5a5372d571a966b1f36bfc141d810b02696e
Simon Maertens <maertens@stce.rwth-aachen.de>
2022-10-05 19:48:52 +0100

    Changed copyright year of LAPACKE_?langb files.

--- LAPACKE/src/lapacke_slangb.c
+++ LAPACKE/src/lapacke_slangb.c
@@ -1,5 +1,5 @@
 /*****************************************************************************
-  Copyright (c) 2014, Intel Corp.
+  Copyright (c) 2022, Intel Corp.
   All rights reserved.
 
   Redistribution and use in source and binary forms, with or without
