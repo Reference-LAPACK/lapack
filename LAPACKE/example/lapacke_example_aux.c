dae4c5a3a44141ce411742aa2f9ea850493dd764
Maria Kraynyuk <maria.kraynyuk@intel.com>
2023-07-27 16:50:55 -0700

    Add LAPACKE examples for extended _64 API

--- LAPACKE/example/lapacke_example_aux.c
+++ LAPACKE/example/lapacke_example_aux.c
@@ -31,3 +31,35 @@ void print_vector( char* desc, lapack_int n, lapack_int* vec ) {
         for( j = 0; j < n; j++ ) printf( " %6" LAPACK_IFMT, vec[j] );
         printf( "\n" );
 }
+
+/* ILP64 routines */
+/* Auxiliary routine: printing a matrix */
+void print_matrix_rowmajor_64( char* desc, int64_t m, int64_t n, double* mat, int64_t ldm ) {
+        int64_t i, j;
+        printf( "\n %s\n", desc );
+
+        for( i = 0; i < m; i++ ) {
+                for( j = 0; j < n; j++ ) printf( " %6.2f", mat[i*ldm+j] );
+                printf( "\n" );
+        }
+}
+
+
+/* Auxiliary routine: printing a matrix */
+void print_matrix_colmajor_64( char* desc, int64_t m, int64_t n, double* mat, int64_t ldm ) {
+        int64_t i, j;
+        printf( "\n %s\n", desc );
+
+        for( i = 0; i < m; i++ ) {
+                for( j = 0; j < n; j++ ) printf( " %6.2f", mat[i+j*ldm] );
+                printf( "\n" );
+        }
+}
+
+/* Auxiliary routine: printing a vector of integers */
+void print_vector_64( char* desc, int64_t n, int64_t* vec ) {
+        int64_t j;
+        printf( "\n %s\n", desc );
+        for( j = 0; j < n; j++ ) printf( " %6" LAPACK_IFMT, vec[j] );
+        printf( "\n" );
+}
