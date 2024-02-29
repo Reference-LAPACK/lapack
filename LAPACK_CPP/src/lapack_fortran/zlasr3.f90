1a95ff9b675b929c6e9ca4bd032f307ce3bb1c1d
thijssteel <thijs.steel@kuleuven.be>
2024-02-29 16:21:40 +0100

    add fortran wrappers for lasr3

---
+++ LAPACK_CPP/src/lapack_fortran/zlasr3.f90
@@ -0,0 +1,32 @@
+subroutine zlasr3 ( side, direct, m, n, k, C, ldc, S, lds, A, lda, work, lwork )
+    use iso_c_binding, only: c_char
+    implicit none
+    ! Interface to C
+    interface
+        subroutine lapack_c_zlasr3(side_c, direct_c, m_c, n_c, k_c, C_c,&
+             ldc_c, S_c, lds_c, A_c, lda_c, work_c, lwork_c) bind(c, name="lapack_c_zlasr3")
+            use iso_c_binding, only: c_char, c_double, c_double_complex, c_int
+            implicit none
+            integer(c_int), intent(in) :: m_c,n_c,k_c,ldc_c, lds_c, lda_c, lwork_c
+            character(c_char), intent(in) :: side_c, direct_c
+            real(c_double), intent(in) :: C_c(ldc_c,*)
+            complex(c_double_complex), intent(in) :: S_c(lds_c,*)
+            complex(c_double_complex), intent(inout) :: A_c(lda_c,*), work_c(*)
+        end subroutine lapack_c_zlasr3
+    end interface
+
+    ! Arguments
+    integer, intent(in) :: m,n,k,ldc, lds, lda, lwork
+    character, intent(in) :: side, direct
+    double precision, intent(in) :: C(ldc,*)
+    double complex, intent(in) :: S(lds,*)
+    double complex, intent(inout) :: A(lda,*), work(*)
+    character(c_char) :: side_to_c, direct_to_c
+
+    ! Convert characters to C characters
+    side_to_c = side
+    direct_to_c = direct
+
+    ! Call C function
+    call lapack_c_zlasr3(side_to_c, direct_to_c, m, n, k, C, ldc, S, lds, A, lda, work, lwork)
+end subroutine zlasr3
