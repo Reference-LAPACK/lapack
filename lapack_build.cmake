0af92d8230f9904735360ed9af6f54d9bd25e4ad
Simon Maertens <simon@slmaertens.dev>
2024-06-05 13:13:38 +0200

    Bump minimum CMake version to 3.13

--- lapack_build.cmake
+++ lapack_build.cmake
@@ -4,7 +4,7 @@
 ## HINTS: ctest -Ddashboard_model=Nightly      -S $(pwd)/lapack/lapack_build.cmake
 ##
 
-cmake_minimum_required(VERSION 3.6)
+cmake_minimum_required(VERSION 3.13)
 ###################################################################
 # The values in this section must always be provided
 ###################################################################
