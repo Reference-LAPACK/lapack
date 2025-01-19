304fa305e85190c934e78eae75c7b092fcfd54c1
Martin Kroeker <martin@ruby.chemie.uni-freiburg.de>
2025-01-19 22:55:52 +0100

    Fix line reflow on addition of _64 symbol suffixes

--- SRC/DEPRECATED/cgelsx.f
+++ SRC/DEPRECATED/cgelsx.f
@@ -364,8 +364,8 @@
 *
 *     B(1:M,1:NRHS) := Q**H * B(1:M,1:NRHS)
 *
-      CALL CUNM2R( 'Left', 'Conjugate transpose', M, NRHS, MN, A, LDA,
-     $             WORK( 1 ), B, LDB, WORK( 2*MN+1 ), INFO )
+      CALL CUNM2R( 'Left', 'Conjugate transpose', M, NRHS, MN, A,
+     $             LDA, WORK( 1 ), B, LDB, WORK( 2*MN+1 ), INFO )
 *
 *     workspace NRHS
 *
