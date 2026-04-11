25d74b31a777a0326bdfd04c4465a8eadde22578
NAKATA Maho <maho.nakata@gmail.com>
2026-04-11 20:29:54 +0900

    TESTING/LIN: use named constants in drvrf tests
      Replace hard-coded floating-point constants with existing named
      constants in the drvrf3 and drvrfp test drivers. This keeps the test
      initialization code consistent with surrounding LAPACK test style.

--- TESTING/LIN/sdrvrf3.f
+++ TESTING/LIN/sdrvrf3.f
@@ -135,9 +135,10 @@
 *  =====================================================================
 *     ..
 *     .. Parameters ..
-      REAL               ZERO, ONE
+      REAL               ZERO, ONE, TWO
       PARAMETER          ( ZERO = ( 0.0E+0, 0.0E+0 ) ,
-     +                     ONE  = ( 1.0E+0, 0.0E+0 ) )
+     +                     ONE  = ( 1.0E+0, 0.0E+0 ),
+     +                     TWO  = 2.0E+0 )
       INTEGER            NTESTS
       PARAMETER          ( NTESTS = 1 )
 *     ..
@@ -283,7 +284,7 @@
                                     DO J = 1, NA
                                        DO I = 1, J
                                           A( I, J ) = A( I, J ) /
-     +                                            ( 2.0 * A( J, J ) )
+     +                                            ( TWO * A( J, J ) )
                                        END DO
                                     END DO
                                  END IF
@@ -306,7 +307,7 @@
                                     DO I = 1, NA
                                        DO J = 1, I
                                           A( I, J ) = A( I, J ) /
-     +                                            ( 2.0 * A( I, I ) )
+     +                                            ( TWO * A( I, I ) )
                                        END DO
                                     END DO
                                  END IF
