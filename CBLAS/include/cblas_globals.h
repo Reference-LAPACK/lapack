5a687f4922a4b7475ba809c2f9d43b342e52316e
Simon Maertens <simon@slmaertens.dev>
2026-04-23 16:43:57 +0200

    Remove the explicit __declspec(dllexport) since that is handles already by CMAKE_WINDOWS_EXPORT_ALL_SYMBOLS

--- CBLAS/include/cblas_globals.h
+++ CBLAS/include/cblas_globals.h
@@ -1,14 +1,8 @@
 #ifndef CBLAS_GLOBALS_H
 #define CBLAS_GLOBALS_H
 
-#if defined(_WIN32)
-  #if defined(CBLAS_DLL_EXPORTS)
-    #define CBLAS_GLOBAL_SYMBOL __declspec(dllexport)
-  #elif defined(CBLAS_DLL_IMPORTS)
-    #define CBLAS_GLOBAL_SYMBOL __declspec(dllimport)
-  #else
-    #define CBLAS_GLOBAL_SYMBOL
-  #endif
+#if defined(CBLAS_DLL_IMPORTS)
+  #define CBLAS_GLOBAL_SYMBOL __declspec(dllimport)
 #else
   #define CBLAS_GLOBAL_SYMBOL
 #endif
