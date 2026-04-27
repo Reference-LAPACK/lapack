01ad09c38bd125da9de57037fc25f4727c235297
Simon Maertens <simon@slmaertens.dev>
2026-04-27 16:19:46 +0200

    New 64bit extended APi generator works

--- CMAKE/ExtendedAPIHelpers.cmake
+++ CMAKE/ExtendedAPIHelpers.cmake
@@ -1,24 +1,33 @@
 include_guard(GLOBAL)
 
-set(EXTENDED_API_GENERATOR "${CMAKE_CURRENT_LIST_DIR}/Generate64BitSuffixedSource.cmake")
+set(EXTENDED_API_GENERATOR
+  "${CMAKE_CURRENT_LIST_DIR}/Generate64BitSuffixedSource.cmake" CACHE INTERNAL
+  "Script that generates 64-bit suffixed sources for the extended API")
+
+if(BUILD_INDEX64_EXT_API)
+  add_custom_target(64bit_codegen ALL
+    COMMENT "Generating 64-bit suffixed sources for extended API")
+endif()
 
 # Generate 64-bit suffixed sources for the extended API. The generation happens
 # at build time in the Generate64BitSuffixedSource.cmake script.
-function(generate_64bit_suffixed_sources gen_target fortran_sources destination generated_sources)
-  get_filename_component(destination_abs "${destination}" ABSOLUTE BASE_DIR "${CMAKE_CURRENT_BINARY_DIR}")
+function(generate_64bit_suffixed_sources target source_list generated_sources)
+  get_filename_component(destination "${target}_64_sources" ABSOLUTE BASE_DIR "${CMAKE_CURRENT_BINARY_DIR}")
   set(generated_source_files)
 
-  foreach(source IN LISTS fortran_sources)
+  foreach(source IN LISTS ${source_list})
     get_filename_component(source_abs "${source}" ABSOLUTE BASE_DIR "${CMAKE_CURRENT_SOURCE_DIR}")
     get_filename_component(source_name "${source}" NAME_WLE)
     get_filename_component(source_ext "${source}" EXT)
-    set(output_file "${destination_abs}/${source_name}_64${source_ext}")
+    set(output_file "${destination}/${source_name}_64${source_ext}")
 
     set(_fortran_extensions ".f" ".F" ".f90" ".F90")
     if(NOT source_ext IN_LIST _fortran_extensions)
-      message(WARNING "Skipping non-Fortran source '${source}' for target '${gen_target}'")
+      message(WARNING "Skipping non-Fortran source '${source}' for target '${target}'")
       continue()
     endif()
+
+    message("${EXTENDED_API_GENERATOR}")
 
     add_custom_command(
       OUTPUT "${output_file}"
@@ -30,14 +39,17 @@ function(generate_64bit_suffixed_sources gen_target fortran_sources destination 
       DEPENDS
         "${source_abs}"
         "${EXTENDED_API_GENERATOR}"
-      COMMENT "Generating ${source_name} for 64-bit extended API"
-      VERBATIM
-    )
+      COMMENT "Generating 64-bit extended API source for ${source}"
+      VERBATIM)
 
     list(APPEND generated_source_files "${output_file}")
   endforeach()
 
   if(generated_source_files)
+    add_custom_target("${target}_64bit_codegen" ALL
+      DEPENDS ${generated_source_files}
+      COMMENT "Generating 64-bit suffixed sources for target ${target}")
+    add_dependencies(64bit_codegen "${target}_64bit_codegen")
     set_source_files_properties(${generated_source_files} PROPERTIES GENERATED TRUE)
   endif()
 
