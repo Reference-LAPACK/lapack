9549c351baba89d7cd1e745b98f8639d158a5399
Simon Maertens <simon@slmaertens.dev>
2026-05-12 15:52:37 +0100

    Fix -Wconversion errors in single complex TESTING/LIN sources during Index-64 builds

--- TESTING/LIN/cdrvrf3.f
+++ TESTING/LIN/cdrvrf3.f
@@ -372,7 +372,7 @@
      +                                            S_WORK_CLANGE )
 *
                               RESULT( 1 ) = RESULT( 1 ) / SQRT( EPS )
-     +                                    / MAX ( MAX( M, N ), 1 )
+     +                                    / REAL( MAX( M, N, 1 ) )
 *
                               IF( RESULT( 1 ).GE.THRESH ) THEN
                                  IF( NFAIL.EQ.0 ) THEN
