acdcf84b5cc3d7d4408a8c968b43705d8bc6cf6e
Simon Maertens <simon@slmaertens.dev>
2026-05-12 12:01:26 +0100

    Fix -Wconversion errors in CBLAS/testing during Index-64 builds

--- CBLAS/testing/c_zblat3.f
+++ CBLAS/testing/c_zblat3.f
@@ -2929,7 +2929,7 @@
          IF( LDC.GT.NMAX )
      $      GO TO 100
          LCC = LDC*N
-         NULL = N.LE.0.
+         NULL = N.LE.0
 *
          DO 90 IK = 1, NIDIM
             K = IDIM( IK )
