ecf39aba107677cc78dd2934b81fa21b0a11b0ce
Ed J <mohawk2@users.noreply.github.com>
2026-09-10 20:07:04 +0100

    add Fortran set_xerbla to BLAS

--- BLAS/SRC/xerbla.f
+++ BLAS/SRC/xerbla.f
@@ -25,6 +25,24 @@
 *> It is called by an LAPACK routine if an input parameter has an
 *> invalid value.  A message is printed and execution stops.
 *>
+*> Users can replace the system XERBLA by calling SET_XERBLA
+*> with a replacement handler that takes the same arguments, which
+*> will then be called on error instead of the above behaviour. This
+*> can then be negated by calling SET_XERBLA with NULL().
+*> The current handler value can be retried by calling GET_XERBLA
+*> with an output argument:
+*>
+*>   PROGRAM HELLO
+*>     EXTERNAL GET_XERBLA
+*>     PROCEDURE(), POINTER :: ALREADY_CB
+*>     INTERFACE
+*>       SUBROUTINE GET_XERBLA(CB_RET)
+*>         PROCEDURE(), POINTER :: CB_RET
+*>       END SUBROUTINE
+*>     END INTERFACE
+*>     CALL GET_XERBLA(ALREADY_CB)
+*>   END PROGRAM HELLO
+*>
 *> Installers may consider modifying the STOP statement in order to
 *> call system-specific exception-handling facilities.
 *> \endverbatim
@@ -57,6 +75,14 @@
 *
 *  =====================================================================
       SUBROUTINE XERBLA( SRNAME, INFO )
+#ifdef LAPACK_ILP64
+#define CB_MODULE XERBLA_CALLBACKS_64
+#define CB_CALLBACK ACTIVE_CALLBACK_64
+#else
+#define CB_MODULE XERBLA_CALLBACKS
+#define CB_CALLBACK ACTIVE_CALLBACK
+#endif
+      USE CB_MODULE, ONLY: CB_CALLBACK
       IMPLICIT NONE
 *
 *  -- Reference BLAS level1 routine --
@@ -74,6 +100,11 @@
       INTRINSIC          LEN_TRIM
 *     ..
 *     .. Executable Statements ..
+*
+      IF (ASSOCIATED(CB_CALLBACK)) THEN
+          CALL CB_CALLBACK(SRNAME, INFO)
+          RETURN
+      END IF
 *
       WRITE( *, FMT = 9999 )SRNAME( 1:LEN_TRIM( SRNAME ) ), INFO
 *
