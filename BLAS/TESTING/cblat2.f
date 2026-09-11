b04d3f9be4cec079776fedd2bb5b97243aa1c4c2
Ed J <mohawk2@users.noreply.github.com>
2026-09-11 01:29:12 +0100

    use SET_BLAS_XERBLA in BLAS tests

--- BLAS/TESTING/cblat2.f
+++ BLAS/TESTING/cblat2.f
@@ -149,7 +149,7 @@
       EXTERNAL           SDIFF, LCE
 *     .. External Subroutines ..
       EXTERNAL           CCHK1, CCHK2, CCHK3, CCHK4, CCHK5, CCHK6,
-     $                   CCHKE, CMVCH
+     $                   CCHKE, CMVCH, SET_BLAS_XERBLA, XER_REPLACE
 *     .. Intrinsic Functions ..
       INTRINSIC          ABS, MAX, MIN
 *     .. Scalars in Common ..
@@ -165,8 +165,21 @@
      $                   'CTRSV ', 'CTBSV ', 'CTPSV ', 'CGERC ',
      $                   'CGERU ', 'CHER  ', 'CHPR  ', 'CHER2 ',
      $                   'CHPR2 '/
+      PROCEDURE(XERBLA_INTERFACE), POINTER :: ALREADY_CB
+      INTERFACE
+        SUBROUTINE XERBLA_INTERFACE(SRNAME, INFO)
+          CHARACTER*(*), INTENT(IN) :: SRNAME
+          INTEGER, INTENT(IN) :: INFO
+        END SUBROUTINE
+        FUNCTION GET_BLAS_XERBLA() RESULT(CB_RET)
+          IMPLICIT NONE
+          PROCEDURE(XERBLA_INTERFACE), POINTER :: CB_RET
+        END FUNCTION
+      END INTERFACE
 *     .. Executable Statements ..
       CALL CPU_TIME( S1 )
+      ALREADY_CB => GET_BLAS_XERBLA()
+      CALL SET_BLAS_XERBLA(XER_REPLACE)
 *
 *     Read name and unit number for summary output file and open file.
 *
@@ -3403,7 +3416,7 @@
   10  CONTINUE
       RETURN
       END
-      SUBROUTINE XERBLA( SRNAME, INFO )
+      SUBROUTINE XER_REPLACE( SRNAME, INFO )
       IMPLICIT NONE
 *
 *  This is a special version of XERBLA to be used only as part of
